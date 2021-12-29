aa1885b65159fe1412c113386d6e7ee953f55a4a
hunto <hunto@foxmail.com>
2021-12-29 17:04:05 +0800

    fix bugs in KLDivergence

--- mmrazor/models/losses/kl_divergence.py
+++ mmrazor/models/losses/kl_divergence.py
@@ -13,35 +13,49 @@ class KLDivergence(nn.Module):
 
     Args:
         tau (float): Temperature coefficient. Defaults to 1.0.
+        reduction (str): Specifies the reduction to apply to the loss:
+            ``'none'`` | ``'batchmean'`` | ``'sum'`` | ``'mean'``.
+            ``'none'``: no reduction will be applied
+            ``'batchmean'``: the sum of the output will be divided by the batchsize
+            ``'sum'``: the output will be summed
+            ``'mean'``: the output will be divided by the number of elements in the output
+            Default: ``'batchmean'``
         loss_weight (float): Weight of loss. Defaults to 1.0.
     """
 
     def __init__(
         self,
         tau=1.0,
+        reduction='batchmean',
         loss_weight=1.0,
     ):
         super(KLDivergence, self).__init__()
         self.tau = tau
         self.loss_weight = loss_weight
 
+        accept_reduction = {'none', 'batchmean', 'sum', 'mean'}
+        assert reduction in accept_reduction, \
+            f'KLDivergence supports reduction {accept_reduction}, ' \
+            f'but gets {reduction}.'
+        self.reduction = reduction
+
     def forward(self, preds_S, preds_T):
         """Forward computation.
 
         Args:
             preds_S (torch.Tensor): The student model prediction with
-                shape (N, C, W, H).
+                shape (N, num_classes).
             preds_T (torch.Tensor): The teacher model prediction with
-                shape (N, C, W, H).
+                shape (N, num_classes).
 
         Return:
             torch.Tensor: The calculated loss value.
         """
-        N, C = preds_S.shape
         preds_T = preds_T.detach()
         softmax_pred_T = F.softmax(preds_T / self.tau, dim=1)
-
-        logsoftmax = torch.nn.LogSoftmax(dim=1)
-        loss = torch.sum(-softmax_pred_T * logsoftmax(preds_S / self.tau)) * (
-            self.tau**2)
-        return self.loss_weight * loss / N
+        logsoftmax_preds_S = F.log_softmax(preds_S / self.tau, dim=1)
+        loss = (self.tau**2) * F.kl_div(
+            logsoftmax_preds_S, 
+            softmax_pred_T, 
+            reduction=self.reduction)
+        return self.loss_weight * loss
