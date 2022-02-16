8174cf51ebb52c3af21155a3d95be2bc157a33f0
pppppM <gjf_mail@126.com>
2022-02-16 15:04:53 +0800

    add mbv2 block and identity

--- mmrazor/models/ops/__init__.py
+++ mmrazor/models/ops/__init__.py
@@ -1,9 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .common import Identity
 from .darts_series import (DartsDilConv, DartsPoolBN, DartsSepConv,
                            DartsSkipConnect, DartsZero)
+from .mbv2_series import MBV2Block
 from .shufflenet_series import ShuffleBlock, ShuffleXception
 
 __all__ = [
     'ShuffleBlock', 'ShuffleXception', 'DartsPoolBN', 'DartsDilConv',
-    'DartsSepConv', 'DartsSkipConnect', 'DartsZero'
+    'DartsSepConv', 'DartsSkipConnect', 'DartsZero', 'MBV2Block', 'Identity'
 ]
