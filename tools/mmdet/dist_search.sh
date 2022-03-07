ef23c624f998078717eddc4a9d38778d0a9393d7
wutongshenqiu <690364065@qq.com>
2022-03-07 21:37:14 +0800

    add PYTHONPATH

--- tools/mmdet/dist_search.sh
+++ tools/mmdet/dist_search.sh
@@ -5,5 +5,6 @@ CHECKPOINT=$2
 GPUS=$3
 PORT=${PORT:-29500}
 
+PYTHONPATH="$(dirname $0)/../..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/search_mmdet.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
--- tools/mmseg/dist_test.sh
+++ tools/mmseg/dist_test.sh
@@ -5,5 +5,6 @@ CHECKPOINT=$2
 GPUS=$3
 PORT=${PORT:-29500}
 
+PYTHONPATH="$(dirname $0)/../..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test_mmseg.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
