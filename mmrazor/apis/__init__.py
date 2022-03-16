90f1a8667247a4663e2cbb95b28a46078421687c
pppppM <gjf_mail@126.com>
2022-03-16 22:27:00 +0800

    add init_random_seed

--- mmrazor/apis/__init__.py
+++ mmrazor/apis/__init__.py
@@ -2,4 +2,6 @@
 from .mmcls import *  # noqa: F401,F403
 from .mmdet import *  # noqa: F401,F403
 from .mmseg import *  # noqa: F401,F403
-from .utils import set_random_seed  # noqa: F401
+from .utils import init_random_seed, set_random_seed  # noqa: F401
+
+__all__ = ['init_random_seed', 'set_random_seed']
