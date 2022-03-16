a43f52ee9e44243a44183684e46102556929d4ad
caoweihan <caoweihan@sensetime.com>
2022-03-16 20:25:23 +0800

    add ModuleInputsRecorder

--- mmrazor/core/recorders/__init__.py
+++ mmrazor/core/recorders/__init__.py
@@ -1,11 +1,13 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .function_outputs_recorder import FunctionOutputsRecorder
 from .method_outputs_recorder import MethodOutputsRecorder
+from .module_inputs_recorder import ModuleInputsRecorder
 from .module_outputs_recorder import ModuleOutputsRecorder
 from .param_recorder import ParameterRecorder
 from .recorder_manager import RecorderManager
 
 __all__ = [
     'FunctionOutputsRecorder', 'MethodOutputsRecorder',
-    'ModuleOutputsRecorder', 'ParameterRecorder', 'RecorderManager'
+    'ModuleOutputsRecorder', 'ParameterRecorder', 'RecorderManager',
+    'ModuleInputsRecorder'
 ]
