5eea6d3038757a5c3d0f52bde817c8b388d007ae
humu789 <humu@pjlab.org.cn>
2022-03-30 17:48:02 +0800

    add auto_scale_lr in configs-mmdet

--- configs/_base_/mmdet_runtime.py
+++ configs/_base_/mmdet_runtime.py
@@ -14,3 +14,11 @@ log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
+
+# Default setting for scaling LR automatically
+#   - The flag `auto_scale_lr` means enable scaling LR automatically
+#       or not by default.
+#   - `default_batch_size` = (8 GPUs) x (2 samples per GPU).
+#   - `default_initial_lr` = The LR by default.
+auto_scale_lr_config = dict(
+    auto_scale_lr=False, default_batch_size=16, default_initial_lr=0.01)
