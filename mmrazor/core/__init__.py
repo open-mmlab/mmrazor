f7c543b77ab57018ff97f738796b01df04345696
pppppM <gjf_mail@126.com>
2022-04-11 18:26:30 +0800

    add recorder registry and build func

--- mmrazor/core/__init__.py
+++ mmrazor/core/__init__.py
@@ -1,10 +1,13 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .builder import build_searcher
+from .builder import build_recorder, build_searcher
 from .distributed_wrapper import DistributedDataParallelWrapper
 from .hooks import *  # noqa: F401,F403
 from .optimizer import *  # noqa: F401,F403
+from .recorders import *  # noqa: F401,F403
 from .runners import *  # noqa: F401,F403
 from .searcher import *  # noqa: F401,F403
 from .utils import *  # noqa: F401,F403
 
-__all__ = ['DistributedDataParallelWrapper', 'build_searcher']
+__all__ = [
+    'DistributedDataParallelWrapper', 'build_searcher', 'build_recorder'
+]
