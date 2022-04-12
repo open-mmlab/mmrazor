35f756c460fcbf52ec412592829c449850e2b08e
caoweihan <caoweihan@sensetime.com>
2022-04-12 15:59:06 +0800

    fix search config

--- configs/pruning/bcnet/search.py
+++ configs/pruning/bcnet/search.py
@@ -7,14 +7,14 @@ algorithm = dict(input_shape=(3, 224, 224))
 searcher = dict(
     type='BCNetSearcher',
     max_channel_bins=20,
-    candidate_pool_size=40,
+    candidate_pool_size=50,
     candidate_top_k=10,
-    constraints=dict(flops=200 * 1e6),
+    constraints=dict(flops=210 * 1e6),
     metrics='accuracy',
     score_key='accuracy_top-1',
-    max_epoch=50,
-    num_mutation=20,
-    num_crossover=20,
+    max_epoch=40,
+    num_mutation=25,
+    num_crossover=25,
     mutate_prob=0.1
 )
 
