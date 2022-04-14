6c5e14fca5da4ab420d4e63cefc36766f50fe865
PJDong <1115957667@qq.com>
2022-04-14 10:02:04 +0800

    [Docs] fix md link failure and lint failture

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -41,12 +41,8 @@ release = get_version()
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx_markdown_tables',
-    'sphinx_copybutton',
-    'myst_parser'
+    'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode',
+    'sphinx_markdown_tables', 'sphinx_copybutton', 'myst_parser'
 ]
 
 autodoc_mock_imports = [
