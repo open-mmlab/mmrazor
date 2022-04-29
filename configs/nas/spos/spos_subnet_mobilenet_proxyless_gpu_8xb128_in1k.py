3f5fa82e1f6b99db42da4f98e9f6ecfb04c8c6e7
whcao <2892770585@qq.com>
2022-04-29 16:22:23 +0800

    fix spos configs

--- configs/nas/spos/spos_subnet_mobilenet_proxyless_gpu_8xb128_in1k.py
+++ configs/nas/spos/spos_subnet_mobilenet_proxyless_gpu_8xb128_in1k.py
@@ -3,7 +3,7 @@ _base_ = [
 ]
 
 # FIXME: you may replace this with the mutable_cfg searched by yourself
-mutable_cfg = 'configs/nas/spos/SPOS_MOBILENET_490M_FROM_ANGELNAS.yaml'
+mutable_cfg = 'https://download.openmmlab.com/mmrazor/v0.1/nas/spos/spos_mobilenet_subnet/spos_angelnas_flops_0.49G_acc_75.98_20220307-54f4698f_mutable_cfg.yaml'  # noqa: E501
 
 algorithm = dict(retraining=True, mutable_cfg=mutable_cfg)
 evaluation = dict(interval=10000, metric='accuracy')
--- configs/nas/spos/spos_subnet_shufflenetv2_8xb128_in1k.py
+++ configs/nas/spos/spos_subnet_shufflenetv2_8xb128_in1k.py
@@ -3,7 +3,7 @@ _base_ = [
 ]
 
 # FIXME: you may replace this with the mutable_cfg searched by yourself
-mutable_cfg = 'configs/nas/spos/SPOS_SHUFFLENETV2_330M_IN1k_PAPER.yaml'
+mutable_cfg = 'https://openmmlab-share.oss-cn-hangzhou.aliyuncs.com/mmrazor/v0.1/nas/spos/spos_shufflenetv2_subnet_8xb128_in1k/spos_shufflenetv2_subnet_8xb128_in1k_flops_0.33M_acc_73.87_20211222-454627be_mutable_cfg.yaml'  # noqa: E501
 
 algorithm = dict(retraining=True, mutable_cfg=mutable_cfg)
 
