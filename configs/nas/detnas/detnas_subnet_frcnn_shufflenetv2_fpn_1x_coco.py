f424117f2381c77cc3f6a8bda50e98b3bc6bb989
whcao <2892770585@qq.com>
2022-04-29 16:43:30 +0800

    replace the official mutable_cfg with the mutable_cfg searched by ourselves

--- configs/nas/detnas/detnas_subnet_frcnn_shufflenetv2_fpn_1x_coco.py
+++ configs/nas/detnas/detnas_subnet_frcnn_shufflenetv2_fpn_1x_coco.py
@@ -1,6 +1,6 @@
 _base_ = ['./detnas_supernet_frcnn_shufflenetv2_fpn_1x_coco.py']
 
 # FIXME: you may replace this with the mutable_cfg searched by yourself
-mutable_cfg = 'configs/nas/detnas/DETNAS_FRCNN_SHUFFLENETV2_340M_COCO_MMRAZOR.yaml'  # noqa: E501
+mutable_cfg = 'https://openmmlab-share.oss-cn-hangzhou.aliyuncs.com/mmrazor/v0.1/nas/detnas/detnas_subnet_frcnn_shufflenetv2_fpn_1x_coco/detnas_subnet_frcnn_shufflenetv2_fpn_1x_coco_bbox_backbone_flops-0.34M_mAP-37.5_20211222-67fea61f_mutable_cfg.yaml'  # noqa: E501
 
 algorithm = dict(retraining=True, mutable_cfg=mutable_cfg)
