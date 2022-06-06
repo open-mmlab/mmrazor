87d54ec39316e4d94f22c340e3c25ea25b2ac5ff
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-06-06 09:52:44 +0800

    add manifest.in

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ def add_mim_extension():
     else:
         return
 
-    filenames = ['tools', 'configs', 'model-index.yml']
+    filenames = ['tools', 'configs', 'demo', 'model-index.yml']
     repo_path = osp.dirname(__file__)
     mim_path = osp.join(repo_path, 'mmrazor', '.mim')
     os.makedirs(mim_path, exist_ok=True)
