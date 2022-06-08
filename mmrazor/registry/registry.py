31b60501c92ee741e8a0ff40e53e30e9661e2e33
pppppM <67539920+pppppM@users.noreply.github.com>
2022-06-08 07:17:27 +0000

    Update registry and fix ci

--- mmrazor/registry/registry.py
+++ mmrazor/registry/registry.py
@@ -14,7 +14,8 @@ from mmengine.registry import METRICS as MMENGINE_METRICS
 from mmengine.registry import MODEL_WRAPPERS as MMENGINE_MODEL_WRAPPERS
 from mmengine.registry import MODELS as MMENGINE_MODELS
 from mmengine.registry import \
-    OPTIMIZER_CONSTRUCTORS as MMENGINE_OPTIMIZER_CONSTRUCTORS
+    OPTIM_WRAPPER_CONSTRUCTORS as MMENGINE_OPTIM_WRAPPER_CONSTRUCTORS
+from mmengine.registry import OPTIM_WRAPPERS as MMENGINE_OPTIM_WRAPPERS
 from mmengine.registry import OPTIMIZERS as MMENGINE_OPTIMIZERS
 from mmengine.registry import PARAM_SCHEDULERS as MMENGINE_PARAM_SCHEDULERS
 from mmengine.registry import \
@@ -28,6 +29,7 @@ from mmengine.registry import \
     WEIGHT_INITIALIZERS as MMENGINE_WEIGHT_INITIALIZERS
 from mmengine.registry import Registry
 
+# Registries For Runner and the related
 # manage all kinds of runners like `EpochBasedRunner` and `IterBasedRunner`
 RUNNERS = Registry('runner', parent=MMENGINE_RUNNERS)
 # manage runner constructors that define how to initialize runners
@@ -38,33 +40,40 @@ LOOPS = Registry('loop', parent=MMENGINE_LOOPS)
 # manage all kinds of hooks like `CheckpointHook`
 HOOKS = Registry('hook', parent=MMENGINE_HOOKS)
 
+# Registries For Data and the related
 # manage data-related modules
 DATASETS = Registry('dataset', parent=MMENGINE_DATASETS)
 DATA_SAMPLERS = Registry('data sampler', parent=MMENGINE_DATA_SAMPLERS)
 TRANSFORMS = Registry('transform', parent=MMENGINE_TRANSFORMS)
 
-# mangage all kinds of modules inheriting `nn.Module`
+# manage all kinds of modules inheriting `nn.Module`
 MODELS = Registry('model', parent=MMENGINE_MODELS)
-# mangage all kinds of model wrappers like 'MMDistributedDataParallel'
+# manage all kinds of model wrappers like 'MMDistributedDataParallel'
 MODEL_WRAPPERS = Registry('model_wrapper', parent=MMENGINE_MODEL_WRAPPERS)
-# mangage all kinds of weight initialization modules like `Uniform`
+# manage all kinds of weight initialization modules like `Uniform`
 WEIGHT_INITIALIZERS = Registry(
     'weight initializer', parent=MMENGINE_WEIGHT_INITIALIZERS)
 
-# mangage all kinds of optimizers like `SGD` and `Adam`
+# Registries For Optimizer and the related
+# manage all kinds of optimizers like `SGD` and `Adam`
 OPTIMIZERS = Registry('optimizer', parent=MMENGINE_OPTIMIZERS)
+# manage optimizer wrapper
+OPTIM_WRAPPERS = Registry('optimizer_wrapper', parent=MMENGINE_OPTIM_WRAPPERS)
 # manage constructors that customize the optimization hyperparameters.
-OPTIMIZER_CONSTRUCTORS = Registry(
-    'optimizer constructor', parent=MMENGINE_OPTIMIZER_CONSTRUCTORS)
-# mangage all kinds of parameter schedulers like `MultiStepLR`
+OPTIM_WRAPPER_CONSTRUCTORS = Registry(
+    'optimizer wrapper constructor',
+    parent=MMENGINE_OPTIM_WRAPPER_CONSTRUCTORS)
+# manage all kinds of parameter schedulers like `MultiStepLR`
 PARAM_SCHEDULERS = Registry(
     'parameter scheduler', parent=MMENGINE_PARAM_SCHEDULERS)
+
 # manage all kinds of metrics
 METRICS = Registry('metric', parent=MMENGINE_METRICS)
 
 # manage task-specific modules like anchor generators and box coders
 TASK_UTILS = Registry('task util', parent=MMENGINE_TASK_UTILS)
 
+# Registries For Visualizer and the related
 # manage visualizer
 VISUALIZERS = Registry('visualizer', parent=MMENGINE_VISUALIZERS)
 # manage visualizer backend
