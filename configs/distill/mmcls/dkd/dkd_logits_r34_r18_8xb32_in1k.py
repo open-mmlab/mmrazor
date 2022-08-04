083c90f0c8be236ca1c520ea71abaca6f263e690
zengyi.vendor <zengyi.vendor@sensetime.com>
2022-08-04 18:42:23 +0800

    linting

--- configs/distill/mmcls/dkd/dkd_logits_r34_r18_8xb32_in1k.py
+++ configs/distill/mmcls/dkd/dkd_logits_r34_r18_8xb32_in1k.py
@@ -18,7 +18,7 @@ model = dict(
         cfg_path='mmcls::resnet/resnet18_8xb32_in1k.py', pretrained=False),
     teacher=dict(
         cfg_path='mmcls::resnet/resnet34_8xb32_in1k.py', pretrained=True),
-    teacher_ckpt='data/razor_checkpoint/resnet34_8xb32_in1k_20210831-f257d4e6.pth',
+    teacher_ckpt='resnet34_8xb32_in1k_20210831-f257d4e6.pth',
     distiller=dict(
         type='ConfigurableDistiller',
         student_recorders=dict(
@@ -30,8 +30,8 @@ model = dict(
             loss_dkd=dict(type='DKDLoss', tau=1, beta=0.5, loss_weight=1)),
         loss_forward_mappings=dict(
             loss_dkd=dict(
-                preds_S=dict(from_student=True, recorder='fc'),
-                preds_T=dict(from_student=False, recorder='fc'),
+                student=dict(from_student=True, recorder='fc'),
+                teacher=dict(from_student=False, recorder='fc'),
                 data_samples=dict(
                     recorder='data_samples', from_student=True, data_idx=1)))))
 
