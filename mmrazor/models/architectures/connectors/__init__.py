05ea47bd3e61a9bb858a3ef39cc8d5d3c7b4a2c0
zengyi.vendor <zengyi.vendor@sensetime.com>
2022-08-09 19:34:36 +0800

    tmpsave

--- mmrazor/models/architectures/connectors/__init__.py
+++ mmrazor/models/architectures/connectors/__init__.py
@@ -2,4 +2,4 @@
 from .byot_connector import BYOTConncetor
 from .convmodule_connector import ConvModuleConncetor
 
-__all__ = ['ConvModuleConncetor', 'BYOTConncetor', 'GTConnector']
+__all__ = ['ConvModuleConncetor', 'BYOTConncetor']
