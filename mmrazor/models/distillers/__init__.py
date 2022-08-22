c6e8dcd209020b1018a5f65cdf6b43253c6c6240
zengyi <31244134+spynccat@users.noreply.github.com>
2022-08-22 14:08:02 +0800

    [Feature] Add BYOT Distillation (#232)
    * byot connector & distiller
    
    * fix config
    
    * fix connector
    
    * tmpsave
    
    * add byot & kdsoftce loss
    
    * update dev-1.x
    
    * fx wsld
    
    * Update README.md
    
    * Update README.md
    
    * fix md
    
    * add ut & REQUIRE REVIEW part
    
    * fix md
    
    * add SelfDistillValLoop UT
    
    * fix comments
    
    * fix comments v2
    
    * fix comments v3
    
    * add connector_idx=None to ConfigurableDistiller.get_record()
    
    Co-authored-by: zengyi.vendor <zengyi.vendor@sensetime.com>

--- mmrazor/models/distillers/__init__.py
+++ mmrazor/models/distillers/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base_distiller import BaseDistiller
+from .byot_distiller import BYOTDistiller
 from .configurable_distiller import ConfigurableDistiller
 
-__all__ = ['ConfigurableDistiller', 'BaseDistiller']
+__all__ = ['ConfigurableDistiller', 'BaseDistiller', 'BYOTDistiller']
--- mmrazor/models/losses/__init__.py
+++ mmrazor/models/losses/__init__.py
@@ -2,6 +2,7 @@
 from .ab_loss import ABLoss
 from .cwd import ChannelWiseDivergence
 from .decoupled_kd import DKDLoss
+from .kd_soft_ce_loss import KDSoftCELoss
 from .kl_divergence import KLDivergence
 from .l2_loss import L2Loss
 from .relational_kd import AngleWiseRKD, DistanceWiseRKD
@@ -9,5 +10,5 @@ from .weighted_soft_label_distillation import WSLD
 
 __all__ = [
     'ChannelWiseDivergence', 'KLDivergence', 'AngleWiseRKD', 'DistanceWiseRKD',
-    'WSLD', 'L2Loss', 'ABLoss', 'DKDLoss'
+    'WSLD', 'L2Loss', 'ABLoss', 'DKDLoss', 'KDSoftCELoss'
 ]
