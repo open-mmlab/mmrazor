1459ee19a8b0dbf116d1d10b3a4d3609e5bcfdc6
huangpengsheng <huangpengsheng@sensetime.com>
2022-08-23 18:25:09 +0800

    init

--- mmrazor/models/losses/__init__.py
+++ mmrazor/models/losses/__init__.py
@@ -6,11 +6,12 @@ from .decoupled_kd import DKDLoss
 from .kd_soft_ce_loss import KDSoftCELoss
 from .kl_divergence import KLDivergence
 from .l2_loss import L2Loss
+from .ofd_loss import OFDLoss
 from .relational_kd import AngleWiseRKD, DistanceWiseRKD
 from .weighted_soft_label_distillation import WSLD
 
 __all__ = [
     'ChannelWiseDivergence', 'KLDivergence', 'AngleWiseRKD', 'DistanceWiseRKD',
     'WSLD', 'L2Loss', 'ABLoss', 'DKDLoss', 'KDSoftCELoss', 'ActivationLoss',
-    'OnehotLikeLoss', 'InformationEntropyLoss'
+    'OnehotLikeLoss', 'InformationEntropyLoss', 'OFDLoss'
 ]
