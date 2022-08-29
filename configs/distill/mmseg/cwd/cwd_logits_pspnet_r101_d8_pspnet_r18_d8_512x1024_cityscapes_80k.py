46fbc2fe9ee0c8d94d825a93d524a057de64e1f2
gaojianfei <gaojianfei@sensetime.com>
2022-08-29 20:13:25 +0800

    fix pspnet config

--- configs/distill/mmseg/cwd/cwd_logits_pspnet_r101_d8_pspnet_r18_d8_512x1024_cityscapes_80k.py
+++ configs/distill/mmseg/cwd/cwd_logits_pspnet_r101_d8_pspnet_r18_d8_512x1024_cityscapes_80k.py
@@ -5,15 +5,13 @@ _base_ = [
 ]
 
 teacher_ckpt = 'https://download.openmmlab.com/mmsegmentation/v0.5/pspnet/pspnet_r101-d8_512x1024_80k_cityscapes/pspnet_r101-d8_512x1024_80k_cityscapes_20200606_112211-e1e1100f.pth'  # noqa: E501
+teacher_cfg_path = 'mmseg::pspnet/pspnet_r101-d8_4xb2-80k_cityscapes-512x1024.py'  # noqa: E501
+student_cfg_path = 'mmseg::pspnet/pspnet_r18-d8_4xb2-80k_cityscapes-512x1024.py'  # noqa: E501
 model = dict(
     _scope_='mmrazor',
     type='SingleTeacherDistill',
-    architecture=dict(
-        cfg_path='mmseg::pspnet/pspnet_r18-d8_512x1024_80k_cityscapes.py',
-        pretrained=False),
-    teacher=dict(
-        cfg_path='mmseg::pspnet/pspnet_r101-d8_512x1024_80k_cityscapes.py',
-        pretrained=False),
+    architecture=dict(cfg_path=student_cfg_path, pretrained=False),
+    teacher=dict(cfg_path=teacher_cfg_path, pretrained=False),
     teacher_ckpt=teacher_ckpt,
     distiller=dict(
         type='ConfigurableDistiller',
