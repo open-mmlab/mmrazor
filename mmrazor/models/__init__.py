8180f13776e904f75eb4b043891cdc4387b8a57b
pppppM <gjf_mail@126.com>
2022-08-31 23:17:29 +0800

    move ops from models to models.architectures

--- mmrazor/models/__init__.py
+++ mmrazor/models/__init__.py
@@ -5,5 +5,4 @@ from .distillers import *  # noqa: F401,F403
 from .losses import *  # noqa: F401,F403
 from .mutables import *  # noqa: F401,F403
 from .mutators import *  # noqa: F401,F403
-from .ops import *  # noqa: F401,F403
 from .task_modules import *  # noqa: F401,F403
