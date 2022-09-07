75d416d41ac9c9b7cce8b653cefaf4240ccf3859
sunyue1 <sunyue1@sensetime.com>
2022-09-07 22:11:59 +0800

    add batch_augments tricks compared to vit

--- configs/nas/mmcls/autoformer/autoformer_supernet_32xb256_in1k.py
+++ configs/nas/mmcls/autoformer/autoformer_supernet_32xb256_in1k.py
@@ -5,13 +5,18 @@ _base_ = [
 
 # data preprocessor
 data_preprocessor = dict(
-    type='ImgDataPreprocessor',
+    type='mmcls.ClsDataPreprocessor',
     # RGB format normalization parameters
     mean=[123.675, 116.28, 103.53],
     std=[58.395, 57.12, 57.375],
     # convert image from BGR to RGB
-    bgr_to_rgb=True,
-)
+    to_rgb=True,
+    batch_augments=dict(
+        augments=[
+            dict(type='Mixup', alpha=0.2, num_classes=1000),
+            dict(type='CutMix', alpha=1.0, num_classes=1000)
+        ],
+        probs=[0.3, 0.5]))
 
 supernet = dict(
     _scope_='mmrazor',
@@ -25,9 +30,9 @@ supernet = dict(
         in_channels=624,
         loss=dict(
             type='mmcls.LabelSmoothLoss',
+            mode='original',
             num_classes=1000,
             label_smooth_val=0.1,
-            mode='original',
             loss_weight=1.0),
         topk=(1, 5)),
 )
@@ -38,8 +43,7 @@ model = dict(
     fix_subnet=None,
     mutators=dict(
         channel_mutator=dict(type='mmrazor.BigNASChannelMutator'),
-        value_mutator=dict(type='mmrazor.DynamicValueMutator')),
-)
+        value_mutator=dict(type='mmrazor.DynamicValueMutator')))
 
 # runtime setting
 custom_hooks = [dict(type='EMAHook', momentum=4e-5, priority='ABOVE_NORMAL')]
