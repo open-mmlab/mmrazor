52e1a1623158869edf6021881003142a828fd320
liukai <liukai@pjlab.org.cn>
2022-09-08 09:54:13 +0800

    restore __init__ of dynamic op

--- mmrazor/models/architectures/dynamic_ops/__init__.py
+++ mmrazor/models/architectures/dynamic_ops/__init__.py
@@ -1,12 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import DynamicOP
-from .default_dynamic_ops import (ChannelDynamicOP, DynamicBatchNorm,
-                                  DynamicConv2d, DynamicGroupNorm,
-                                  DynamicInstanceNorm, DynamicLinear)
+from .default_dynamic_ops import (DynamicBatchNorm, DynamicConv2d,
+                                  DynamicGroupNorm, DynamicInstanceNorm,
+                                  DynamicLinear)
 from .slimmable_dynamic_ops import SwitchableBatchNorm2d
 
 __all__ = [
     'DynamicConv2d', 'DynamicLinear', 'DynamicBatchNorm',
     'DynamicInstanceNorm', 'DynamicGroupNorm', 'SwitchableBatchNorm2d',
-    'DynamicOP', 'ChannelDynamicOP'
+    'DynamicOP'
 ]
