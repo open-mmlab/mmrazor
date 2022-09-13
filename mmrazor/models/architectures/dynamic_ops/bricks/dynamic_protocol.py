6401d277bb475051cb819d6c44498ce855c88743
aptsunny <aptsunny@tongji.edu.cn>
2022-09-13 09:14:05 +0800

    add dynamicRP support for DynamicMultiheadAttention

---
+++ mmrazor/models/architectures/dynamic_ops/bricks/dynamic_protocol.py
@@ -0,0 +1,37 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+
+import sys
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Protocol
+else:
+    from typing import Protocol
+
+import torch.nn as nn
+
+from mmrazor.models.mutables.base_mutable import BaseMutable
+from mmrazor.models.architectures.ops import RelativePosition2D
+
+
+class DynamicMHAProtocol(Protocol):
+    """Protocol for Dynamic Multi head Attention."""
+    relative_position: bool
+    max_relative_position: int
+    rel_pos_embed_k: RelativePosition2D
+    rel_pos_embed_v: RelativePosition2D
+    w_qs: nn.Linear
+    w_ks: nn.Linear
+    w_vs: nn.Linear
+    embed_dims: int
+    proj: nn.Module
+    attn_drop_rate: float
+
+
+class DynamicRPProtocol(Protocol):
+    """Protocol for Dynamic Relative Position."""
+    mutable_attrs: nn.ModuleDict
+    mutable_head_dims: BaseMutable
+    head_dims: int
+    max_relative_position: int
+    embeddings_table_v: nn.Parameter
+    embeddings_table_h: nn.Parameter
