93dcf8c63638d41fa5bb2256f734bf959a7fe5b1
humu789 <humu@pjlab.org.cn>
2022-09-20 16:42:05 +0800

    pre-rebase

--- mmrazor/models/fake_quants/__init__.py
+++ mmrazor/models/fake_quants/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseFakeQuantize
+from .adaround import AdaRoundFakeQuantize
 
-__all__ = ['BaseFakeQuantize']
+__all__ = ['BaseFakeQuantize', 'AdaRoundFakeQuantize']
--- mmrazor/models/fake_quants/adaround.py
+++ mmrazor/models/fake_quants/adaround.py
@@ -1,27 +1,25 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 import torch
-from torch.nn.parameter import Parameter
-
+from torch.ao.quantization import FakeQuantize
 from mmrazor.registry import MODELS
-from .base import FakeQuantize
-
-_version_under_1100 = int(torch.__version__.split('.')[1]) < 10
-
+from mmrazor.models.utils import (_is_per_channel, _is_per_tensor, 
+    _is_symmetric_quant, _is_float_qparams) 
+from mmrazor.models.observers import MinMaxObserver
+from .base import BaseFakeQuantize
 
 @MODELS.register_module()
-class AdaRoundFakeQuantize(FakeQuantize):
-
-    def __init__(self, observer, **observer_kwargs):
-        super().__init__(observer, **observer_kwargs)
+class AdaRoundFakeQuantize(BaseFakeQuantize):
+    
+    def __init__(observer):
+        super().__init__(observer)
         self.adaround = False
-
+    
     def init(self, weight_tensor: torch.Tensor):
         self.adaround = True
         self.observer_enabled[0] = 0
         self.fake_quant_enabled[0] = 1
 
         # self.soft_targets = False  # delete this
-        self.gamma = -0.1
+        self.gamma = -0.1 
         self.zeta = 1.1
         self.init_alpha(x=weight_tensor.data.clone())
 
@@ -34,13 +32,11 @@ class AdaRoundFakeQuantize(FakeQuantize):
             scale = self.scale.data
         x_floor = torch.floor(x / scale)
         rest = (x / scale) - x_floor  # rest of rounding [0, 1)
-        alpha = -torch.log((self.zeta - self.gamma) /
-                           (rest - self.gamma) - 1)  # => sigmoid(alpha) = rest
+        alpha = -torch.log((self.zeta - self.gamma) / (rest - self.gamma) - 1)  # => sigmoid(alpha) = rest
         self.alpha = Parameter(alpha)
 
     def rectified_sigmoid(self):
         """Function to generate rounding mask.
-
         Args:
             x (torch.Tensor):
             zeta (torch.Tensor):
@@ -48,8 +44,7 @@ class AdaRoundFakeQuantize(FakeQuantize):
         Returns:
             torch.Tensor:
         """
-        return ((self.zeta - self.gamma) * torch.sigmoid(self.alpha) +
-                self.gamma).clamp(0, 1)
+        return ((self.zeta - self.gamma) * torch.sigmoid(self.alpha) + self.gamma).clamp(0, 1)
 
     def adaround_forward(self, x, hard_value=False):
         if self.ch_axis != -1:
@@ -66,13 +61,12 @@ class AdaRoundFakeQuantize(FakeQuantize):
         x = torch.clamp(x, self.quant_min, self.quant_max)
         x = (x - zero_point) * scale
         return x
-
+    
     def forward(self, X):
         if self.observer_enabled[0] == 1:
             self.activation_post_process(X.detach())
             _scale, _zero_point = self.calculate_qparams()
-            _scale, _zero_point = _scale.to(self.scale.device), _zero_point.to(
-                self.zero_point.device)
+            _scale, _zero_point = _scale.to(self.scale.device), _zero_point.to(self.zero_point.device)
             if self.scale.shape != _scale.shape:
                 self.scale.resize_(_scale.shape)
                 self.zero_point.resize_(_zero_point.shape)
@@ -84,8 +78,7 @@ class AdaRoundFakeQuantize(FakeQuantize):
                 if self.is_per_channel:
                     X = torch.fake_quantize_per_channel_affine(
                         X, self.scale,
-                        self.zero_point.long()
-                        if _version_under_1100 else self.zero_point,
+                        self.zero_point.long() if _version_under_1100 else self.zero_point,
                         self.ch_axis, self.quant_min, self.quant_max)
                 else:
                     X = torch.fake_quantize_per_tensor_affine(
