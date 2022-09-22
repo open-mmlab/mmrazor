63fd1f674cbeba9820d5b9b5fb60f2bf5c355206
liukai <liukai@pjlab.org.cn>
2022-09-22 16:18:08 +0800

    clean old mutable_channels

--- mmrazor/models/mutables/mutable_channel/__init__.py
+++ mmrazor/models/mutables/mutable_channel/__init__.py
@@ -8,13 +8,18 @@ from .mutable_channel_container import MutableChannelContainer
 from .one_shot_mutable_channel import OneShotMutableChannel
 from .sequential_mutable_channel import SquentialMutableChannel
 from .simple_mutable_channel import SimpleMutableChannel
-from .slimmable_mutable_channel import SlimmableMutableChannel
 
 __all__ = [
-    'SimpleMutableChannel', 'L1MutableChannelGroup',
-    'SequentialMutableChannelGroup', 'MutableChannelGroup',
-    'OneShotMutableChannelGroup', 'SlimmableChannelGroup',
-    'BaseMutableChannel', 'MutableChannelContainer', 'SquentialMutableChannel',
-    'ChannelGroupType', 'MutableChannel', 'OneShotMutableChannel',
-    'SlimmableMutableChannel'
+    'SimpleMutableChannel',
+    'L1MutableChannelGroup',
+    'SequentialMutableChannelGroup',
+    'MutableChannelGroup',
+    'OneShotMutableChannelGroup',
+    'SlimmableChannelGroup',
+    'BaseMutableChannel',
+    'MutableChannelContainer',
+    'SquentialMutableChannel',
+    'ChannelGroupType',
+    'MutableChannel',
+    'OneShotMutableChannel',
 ]
