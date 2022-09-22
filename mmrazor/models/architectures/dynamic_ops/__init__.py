b9ee1c90efa61ce8fece3d31a639e989eff43328
liukai <liukai@pjlab.org.cn>
2022-09-22 15:17:08 +0800

    remove old dynamic ops

--- mmrazor/models/architectures/dynamic_ops/__init__.py
+++ mmrazor/models/architectures/dynamic_ops/__init__.py
@@ -1,12 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import DynamicOP
-from .default_dynamic_ops import (DynamicBatchNorm, DynamicConv2d,
-                                  DynamicGroupNorm, DynamicInstanceNorm,
-                                  DynamicLinear)
-from .slimmable_dynamic_ops import SwitchableBatchNorm2d
 
-__all__ = [
-    'DynamicConv2d', 'DynamicLinear', 'DynamicBatchNorm',
-    'DynamicInstanceNorm', 'DynamicGroupNorm', 'SwitchableBatchNorm2d',
-    'DynamicOP'
-]
+# from .default_dynamic_ops import (DynamicBatchNorm, DynamicConv2d,
+#   DynamicGroupNorm, DynamicInstanceNorm,
+#   DynamicLinear)
+
+__all__ = ['DynamicOP']
