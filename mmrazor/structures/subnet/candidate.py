8922a7e59d120508783eae05b5898d53a3dbfe8a
Yue Sun <aptsunny@tongji.edu.cn>
2022-10-14 11:52:59 +0800

    Revert "[Improvement] Support for candidate multiple dimensional search constraints."

--- mmrazor/structures/subnet/candidate.py
+++ mmrazor/structures/subnet/candidate.py
@@ -1,41 +1,33 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from collections import UserList
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 
 class Candidates(UserList):
-    """The data structure of sampled candidate. The format is Union[Dict[Any,
-    Dict], List[Dict[Any, Dict]]].
+    """The data structure of sampled candidate. The format is [(any, float),
+    (any, float), ...].
 
     Examples:
         >>> candidates = Candidates()
-        >>> subnet_1 = {'1': 'choice1', '2': 'choice2'}
+        >>> subnet_1 = {'choice_1': 'layer_1', 'choice_2': 'layer_2'}
         >>> candidates.append(subnet_1)
         >>> candidates
-        [{"{'1': 'choice1', '2': 'choice2'}":
-        {'score': 0.0, 'flops': 0.0, 'params': 0.0, 'latency': 0.0}}]
-        >>> candidates.set_resources(0, 49.9, 'flops')
-        >>> candidates.set_score(0, 100.)
+        [({'choice_1': 'layer_1', 'choice_2': 'layer_2'}, 0.0)]
+        >>> candidates.set_score(0, 0.9)
         >>> candidates
-        [{"{'1': 'choice1', '2': 'choice2'}":
-        {'score': 100.0, 'flops': 49.9, 'params': 0.0, 'latency': 0.0}}]
+        [({'choice_1': 'layer_1', 'choice_2': 'layer_2'}, 0.9)]
         >>> subnet_2 = {'choice_3': 'layer_3', 'choice_4': 'layer_4'}
-        >>> candidates.append(subnet_2)
+        >>> candidates.append((subnet_2, 0.5))
         >>> candidates
-        [{"{'1': 'choice1', '2': 'choice2'}":
-        {'score': 100.0, 'flops': 49.9, 'params': 0.0, 'latency': 0.0}},
-        {"{'choice_3': 'layer_3', 'choice_4':'layer_4'}":
-        {'score': 0.0, 'flops': 0.0, 'params': 0.0, 'latency': 0.0}}]
+        [({'choice_1': 'layer_1', 'choice_2': 'layer_2'}, 0.9),
+        ({'choice_3': 'layer_3', 'choice_4': 'layer_4'}, 0.5)]
         >>> candidates.subnets
-        [{'1': 'choice1', '2': 'choice2'},
+        [{'choice_1': 'layer_1', 'choice_2': 'layer_2'},
         {'choice_3': 'layer_3', 'choice_4': 'layer_4'}]
-        >>> candidates.resources('flops')
-        [49.9, 0.0]
         >>> candidates.scores
-        [100.0, 0.0]
+        [0.9, 0.5]
     """
-    _format_return = Union[Dict[Any, Dict], List[Dict[Any, Dict]]]
-    _indicators = ('score', 'flops', 'params', 'latency')
+    _format_return = Union[Tuple[Any, float], List[Tuple[Any, float]]]
 
     def __init__(self, initdata: Optional[Any] = None):
         self.data = []
@@ -49,44 +41,23 @@ class Candidates(UserList):
     @property
     def scores(self) -> List[float]:
         """The scores of candidates."""
-        return [
-            value.get('score', 0.) for item in self.data
-            for _, value in item.items()
-        ]
-
-    def resources(self, key_indicator: str = 'flops') -> List[float]:
-        """The resources of candidates."""
-        assert key_indicator in ['flops', 'params', 'latency']
-        return [
-            value.get(key_indicator, 0.) for item in self.data
-            for _, value in item.items()
-        ]
+        return [item[1] for item in self.data]
 
     @property
     def subnets(self) -> List[Dict]:
         """The subnets of candidates."""
-        return [eval(key) for item in self.data for key, _ in item.items()]
+        return [item[0] for item in self.data]
 
     def _format(self, data: Any) -> _format_return:
-        """Transform [Dict, ...] to Union[Dict[Any, Dict], List[Dict[Any,
-        Dict]]].
+        """Transform [any, ...] to [tuple(any, float), ...] Transform any to
+        tuple(any, float)."""
 
-        Three types of input are supported:
-            1. Dict.
-            2. Dict[Any, Dict].
-            3. List[Dict[Any, Dict]].
-        """
-
-        def _format_item(cond: Any):
-            """Transform Dict to str(Dict)."""
-            if len(cond.keys()) > 1 and isinstance(
-                    list(cond.values())[0], str):
-                return {str(cond): {}.fromkeys(self._indicators, -1)}
+        def _format_item(item: Any):
+            """Transform any to tuple(any, float)."""
+            if isinstance(item, tuple):
+                return (item[0], float(item[1]))
             else:
-                for value in list(cond.values()):
-                    for key in list(self._indicators):
-                        value.setdefault(key, 0.)
-                return cond
+                return (item, 0.)
 
         if isinstance(data, UserList):
             return [_format_item(i) for i in data.data]
@@ -117,22 +88,4 @@ class Candidates(UserList):
 
     def set_score(self, i: int, score: float) -> None:
         """Set score to the specified subnet by index."""
-        for _, value in self.data[i].items():
-            value['score'] = score
-
-    def set_resources(self,
-                      i: int,
-                      resources: float,
-                      key_indicator: str = 'flops') -> None:
-        """Set resources to the specified subnet by index."""
-        assert key_indicator in ['flops', 'params', 'latency']
-        for _, value in self.data[i].items():
-            value[key_indicator] = resources
-
-    def sort_by(self, key_indicator='score', reverse=True):
-        """Sort by a specific indicator.
-
-        Default score.
-        """
-        self.data.sort(
-            key=lambda x: list(x.values())[0][key_indicator], reverse=reverse)
+        self.data[i] = (self.data[i][0], float(score))
