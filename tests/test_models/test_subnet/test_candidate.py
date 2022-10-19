c168b92e049f793b547386d44d2d3f6252df6126
Yue Sun <aptsunny@tongji.edu.cn>
2022-10-19 09:50:39 +0800

    [Improvement] Update Candidate with multi-dim search constraints. (#322)
    * update doc
    
    * add support type
    
    * clean code
    
    * update candidates
    
    * clean
    
    * xx
    
    * set_resource -> set_score
    
    * fix ci bug
    
    * py36 lint
    
    * fix bug
    
    * fix check constrain
    
    * py36 ci
    
    * redesign candidate
    
    * fix pre-commit
    
    * update cfg
    
    * add build_resource_estimator
    
    * fix ci bug
    
    * remove runner.epoch in testcase

--- tests/test_models/test_subnet/test_candidate.py
+++ tests/test_models/test_subnet/test_candidate.py
@@ -10,7 +10,27 @@ class TestCandidates(TestCase):
 
     def setUp(self) -> None:
         self.fake_subnet = {'1': 'choice1', '2': 'choice2'}
-        self.fake_subnet_with_score = (self.fake_subnet, 1.)
+        self.fake_subnet_with_resource = {
+            str(self.fake_subnet): {
+                'score': 0.,
+                'flops': 50.,
+                'params': 0.,
+                'latency': 0.
+            }
+        }
+        self.fake_subnet_with_score = {
+            str(self.fake_subnet): {
+                'score': 99.,
+                'flops': 0.,
+                'params': 0.,
+                'latency': 0.
+            }
+        }
+        self.has_flops_network = {
+            str(self.fake_subnet): {
+                'flops': 50.,
+            }
+        }
 
     def test_init(self):
         # initlist is None
@@ -23,16 +43,25 @@ class TestCandidates(TestCase):
         # initlist is UserList
         data = UserList([self.fake_subnet] * 2)
         self.assertEqual(len(candidates.data), 2)
+        self.assertEqual(candidates.resources('flops'), [-1, -1])
+        # initlist is list(Dict[str, Dict])
+        candidates = Candidates([self.has_flops_network] * 2)
+        self.assertEqual(candidates.resources('flops'), [50., 50.])
 
     def test_scores(self):
         # test property: scores
         data = [self.fake_subnet_with_score] * 2
         candidates = Candidates(data)
-        self.assertEqual(candidates.scores, [1., 1.])
+        self.assertEqual(candidates.scores, [99., 99.])
+
+    def test_resources(self):
+        data = [self.fake_subnet_with_resource] * 2
+        candidates = Candidates(data)
+        self.assertEqual(candidates.resources('flops'), [50., 50.])
 
     def test_subnets(self):
         # test property: subnets
-        data = [self.fake_subnet_with_score] * 2
+        data = [self.fake_subnet] * 2
         candidates = Candidates(data)
         self.assertEqual(candidates.subnets, [self.fake_subnet] * 2)
 
@@ -41,17 +70,20 @@ class TestCandidates(TestCase):
         candidates = Candidates()
         candidates.append(self.fake_subnet)
         self.assertEqual(len(candidates), 1)
-        # item is tuple
+        # item is List
         candidates = Candidates()
-        candidates.append(self.fake_subnet_with_score)
-        self.assertEqual(len(candidates), 1)
+        candidates.append([self.fake_subnet_with_score])
+        # item is Candidates
+        candidates_2 = Candidates([self.fake_subnet_with_resource])
+        candidates.append(candidates_2)
+        self.assertEqual(len(candidates), 2)
 
     def test_insert(self):
         # item is dict
-        candidates = Candidates([self.fake_subnet_with_score])
+        candidates = Candidates(self.fake_subnet_with_score)
         candidates.insert(1, self.fake_subnet)
         self.assertEqual(len(candidates), 2)
-        # item is tuple
+        # item is List
         candidates = Candidates([self.fake_subnet_with_score])
         candidates.insert(1, self.fake_subnet_with_score)
         self.assertEqual(len(candidates), 2)
@@ -61,13 +93,60 @@ class TestCandidates(TestCase):
         candidates = Candidates([self.fake_subnet_with_score])
         candidates.extend([self.fake_subnet])
         self.assertEqual(len(candidates), 2)
-        # other is UserList
+        # other is Candidates
         candidates = Candidates([self.fake_subnet_with_score])
-        candidates.extend(UserList([self.fake_subnet_with_score]))
+        candidates_2 = Candidates([self.fake_subnet_with_resource])
+        candidates.extend(candidates_2)
         self.assertEqual(len(candidates), 2)
 
-    def test_set_score(self):
-        # test set_score
+    def test_set_resource(self):
+        # test set_resource
+        candidates = Candidates([self.fake_subnet])
+        for kk in ['flops', 'params', 'latency']:
+            self.assertEqual(candidates.resources(kk)[0], -1)
+            candidates.set_resource(0, 49.9, kk)
+            self.assertEqual(candidates.resources(kk)[0], 49.9)
+        candidates.insert(0, self.fake_subnet_with_resource)
+        self.assertEqual(len(candidates), 2)
+        self.assertEqual(candidates.resources('flops'), [50., 49.9])
+        self.assertEqual(candidates.resources('latency'), [0., 49.9])
         candidates = Candidates([self.fake_subnet_with_score])
-        candidates.set_score(0, 0.5)
-        self.assertEqual(candidates[0][1], 0.5)
+        candidates.set_resource(0, 100.0, 'score')
+        self.assertEqual(candidates.scores[0], 100.)
+        candidates = Candidates([self.fake_subnet_with_score])
+        candidates.set_resource(0, 100.0, 'score')
+        candidates.extend(UserList([self.fake_subnet_with_resource]))
+        candidates.set_resource(1, 99.9, 'score')
+        self.assertEqual(candidates.scores, [100., 99.9])
+
+    def test_update_resources(self):
+        # test update_resources
+        candidates = Candidates([self.fake_subnet])
+        candidates.append([self.fake_subnet_with_score])
+        candidates_2 = Candidates(self.fake_subnet_with_resource)
+        candidates.append(candidates_2)
+        self.assertEqual(len(candidates), 3)
+        self.assertEqual(candidates.resources('flops'), [-1, 0., 50.])
+        self.assertEqual(candidates.resources('latency'), [-1, 0., 0.])
+        resources = [{'flops': -2}, {'latency': 4.}]
+        candidates.update_resources(resources, start=1)
+        self.assertEqual(candidates.resources('flops'), [-1, -2, 50.])
+        self.assertEqual(candidates.resources('latency'), [-1, 0., 4])
+        candidates.update_resources(resources, start=0)
+        self.assertEqual(candidates.resources('flops'), [-2, -2, 50.])
+        self.assertEqual(candidates.resources('latency'), [-1, 4., 4.])
+
+    def test_sort(self):
+        # test set_sort
+        candidates = Candidates([self.fake_subnet_with_score])
+        candidates.extend(UserList([self.fake_subnet_with_resource]))
+        candidates.insert(0, self.fake_subnet)
+        candidates.set_resource(0, 100., 'score')
+        candidates.set_resource(2, 98., 'score')
+        self.assertEqual(candidates.scores, [100., 99., 98.])
+        candidates.sort_by(key_indicator='score', reverse=False)
+        self.assertEqual(candidates.scores, [98., 99., 100.])
+        candidates.sort_by(key_indicator='latency')
+        self.assertEqual(candidates.scores, [98., 99., 100.])
+        candidates.sort_by(key_indicator='flops', reverse=False)
+        self.assertEqual(candidates.scores, [100., 99., 98.])
--- tests/test_runners/test_evolution_search_loop.py
+++ tests/test_runners/test_evolution_search_loop.py
@@ -82,7 +82,7 @@ class TestEvolutionSearchLoop(TestCase):
             num_mutation=2,
             num_crossover=2,
             mutate_prob=0.1,
-            flops_range=None,
+            constraints_range=dict(flops=(0, 330)),
             score_key='coco/bbox_mAP')
         self.train_cfg = Config(train_cfg)
         self.runner = MagicMock(spec=ToyRunner)
@@ -103,7 +103,7 @@ class TestEvolutionSearchLoop(TestCase):
 
         # test init_candidates is not None
         fake_subnet = {'1': 'choice1', '2': 'choice2'}
-        fake_candidates = Candidates((fake_subnet, 0.))
+        fake_candidates = Candidates(fake_subnet)
         init_candidates_path = os.path.join(self.temp_dir, 'candidates.yaml')
         fileio.dump(fake_candidates, init_candidates_path)
         loop_cfg.init_candidates = init_candidates_path
@@ -111,8 +111,12 @@ class TestEvolutionSearchLoop(TestCase):
         self.assertIsInstance(loop, EvolutionSearchLoop)
         self.assertEqual(loop.candidates, fake_candidates)
 
-    @patch('mmrazor.engine.runner.evolution_search_loop.export_fix_subnet')
-    def test_run_epoch(self, mock_export_fix_subnet):
+    @patch('mmrazor.engine.runner.utils.check.load_fix_subnet')
+    @patch('mmrazor.engine.runner.utils.check.export_fix_subnet')
+    @patch('mmrazor.models.task_modules.estimators.resource_estimator.'
+           'get_model_flops_params')
+    def test_run_epoch(self, flops_params, mock_export_fix_subnet,
+                       load_status):
         # test_run_epoch: distributed == False
         loop_cfg = copy.deepcopy(self.train_cfg)
         loop_cfg.runner = self.runner
@@ -120,20 +124,20 @@ class TestEvolutionSearchLoop(TestCase):
         loop_cfg.evaluator = self.evaluator
         loop = LOOPS.build(loop_cfg)
         self.runner.rank = 0
-        loop._epoch = 1
         self.runner.distributed = False
         self.runner.work_dir = self.temp_dir
         fake_subnet = {'1': 'choice1', '2': 'choice2'}
-        self.runner.model.sample_subnet = MagicMock(return_value=fake_subnet)
+        loop.model.sample_subnet = MagicMock(return_value=fake_subnet)
+        load_status.return_value = True
+        flops_params.return_value = 0, 0
         loop.run_epoch()
         self.assertEqual(len(loop.candidates), 4)
         self.assertEqual(len(loop.top_k_candidates), 2)
-        self.assertEqual(loop._epoch, 2)
+        self.assertEqual(loop._epoch, 1)
 
         # test_run_epoch: distributed == True
         loop = LOOPS.build(loop_cfg)
         self.runner.rank = 0
-        loop._epoch = 1
         self.runner.distributed = True
         self.runner.work_dir = self.temp_dir
         fake_subnet = {'1': 'choice1', '2': 'choice2'}
@@ -141,26 +145,27 @@ class TestEvolutionSearchLoop(TestCase):
         loop.run_epoch()
         self.assertEqual(len(loop.candidates), 4)
         self.assertEqual(len(loop.top_k_candidates), 2)
-        self.assertEqual(loop._epoch, 2)
+        self.assertEqual(loop._epoch, 1)
 
         # test_check_constraints
-        loop_cfg.flops_range = (0, 100)
+        loop_cfg.constraints_range = dict(params=(0, 100))
         loop = LOOPS.build(loop_cfg)
         self.runner.rank = 0
-        loop._epoch = 1
         self.runner.distributed = True
         self.runner.work_dir = self.temp_dir
         fake_subnet = {'1': 'choice1', '2': 'choice2'}
         loop.model.sample_subnet = MagicMock(return_value=fake_subnet)
-        loop._check_constraints = MagicMock(return_value=True)
+        flops_params.return_value = (50., 1)
         mock_export_fix_subnet.return_value = fake_subnet
         loop.run_epoch()
         self.assertEqual(len(loop.candidates), 4)
         self.assertEqual(len(loop.top_k_candidates), 2)
-        self.assertEqual(loop._epoch, 2)
+        self.assertEqual(loop._epoch, 1)
 
-    @patch('mmrazor.engine.runner.evolution_search_loop.export_fix_subnet')
-    def test_run(self, mock_export_fix_subnet):
+    @patch('mmrazor.engine.runner.utils.check.export_fix_subnet')
+    @patch('mmrazor.models.task_modules.estimators.resource_estimator.'
+           'get_model_flops_params')
+    def test_run_loop(self, mock_flops, mock_export_fix_subnet):
         # test a new search: resume == None
         loop_cfg = copy.deepcopy(self.train_cfg)
         loop_cfg.runner = self.runner
@@ -169,16 +174,26 @@ class TestEvolutionSearchLoop(TestCase):
         loop = LOOPS.build(loop_cfg)
         self.runner.rank = 0
         loop._epoch = 1
+
         fake_subnet = {'1': 'choice1', '2': 'choice2'}
         self.runner.work_dir = self.temp_dir
         loop.update_candidate_pool = MagicMock()
         loop.val_candidate_pool = MagicMock()
+
+        mutation_candidates = Candidates([fake_subnet] * loop.num_mutation)
+        for i in range(loop.num_mutation):
+            mutation_candidates.set_resource(i, 0.1 + 0.1 * i, 'flops')
+            mutation_candidates.set_resource(i, 99 + i, 'score')
+        crossover_candidates = Candidates([fake_subnet] * loop.num_crossover)
+        for i in range(loop.num_crossover):
+            crossover_candidates.set_resource(i, 0.1 + 0.1 * i, 'flops')
+            crossover_candidates.set_resource(i, 99 + i, 'score')
         loop.gen_mutation_candidates = \
-            MagicMock(return_value=[fake_subnet]*loop.num_mutation)
+            MagicMock(return_value=mutation_candidates)
         loop.gen_crossover_candidates = \
-            MagicMock(return_value=[fake_subnet]*loop.num_crossover)
-        loop.top_k_candidates = Candidates([(fake_subnet, 1.0),
-                                            (fake_subnet, 0.9)])
+            MagicMock(return_value=crossover_candidates)
+        loop.candidates = Candidates([fake_subnet] * 4)
+        mock_flops.return_value = (0.5, 101)
         mock_export_fix_subnet.return_value = fake_subnet
         loop.run()
         assert os.path.exists(
