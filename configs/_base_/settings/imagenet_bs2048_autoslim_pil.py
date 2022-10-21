5943d0638aa9a0c03e2698c5868cc241d72d54b8
HIT-cwh <2892770585@qq.com>
2022-10-21 17:36:47 +0800

    fix cfg files

--- configs/_base_/settings/imagenet_bs2048_autoslim_pil.py
+++ configs/_base_/settings/imagenet_bs2048_autoslim_pil.py
@@ -1,9 +1,9 @@
 _base_ = 'imagenet_bs2048_autoslim.py'
 
 _RandomResizedCrop_cfg = _base_.train_dataloader.dataset.pipeline[1]
-assert _RandomResizedCrop_cfg.type == 'mmcls.RandomResizedCrop'
+assert _RandomResizedCrop_cfg.type == 'RandomResizedCrop'
 _RandomResizedCrop_cfg.backend = 'pillow'
 
 _ResizeEdge_cfg = _base_.test_dataloader.dataset.pipeline[1]
-assert _ResizeEdge_cfg.type == 'mmcls.ResizeEdge'
+assert _ResizeEdge_cfg.type == 'ResizeEdge'
 _ResizeEdge_cfg.backend = 'pillow'
