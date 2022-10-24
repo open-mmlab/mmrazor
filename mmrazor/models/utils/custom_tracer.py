55c5499cc0750bdc948f45b9fddd29946a3d31ac
huangpengsheng <huangpengsheng@sensetime.com>
2022-10-24 15:32:26 +0800

    updated

--- mmrazor/models/utils/custom_tracer.py
+++ mmrazor/models/utils/custom_tracer.py
@@ -16,7 +16,7 @@ _orig_module_getattr: Callable = nn.Module.__getattr__
 # _orig_module_forward_train: Callable = models.BaseDenseHead.forward_train
 
 
-class TracedMethodRegistry:
+class UntracedMethodRegistry:
     method_dict: Dict[str, nn.Module] = dict()
     tracer = None
 
@@ -50,6 +50,16 @@ class TracedMethodRegistry:
         return wrapped_method
 
 
+def register_skipped_method(method):
+    if method is not list:
+        method = [method]
+    for met in method:
+        mod = met.__module__
+        _registry = UntracedMethodRegistry(met)
+        UntracedMethodRegistry.method_dict[met.__name__] = dict(
+            mod=mod, wrapped=_registry.method_wrapper())
+
+
 def custom_symbolic_tracer(root, concrete_args=None):
     tracer = CustomTracer()
     graph = tracer.trace(root, concrete_args)
@@ -60,9 +70,36 @@ def custom_symbolic_tracer(root, concrete_args=None):
 
 class CustomTracer(Tracer):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,
+                 *args,
+                 customed_leaf_module=None,
+                 customed_skipped_module=None,
+                 **kwargs):
         super(CustomTracer, self).__init__()
-        TracedMethodRegistry.tracer = self
+        UntracedMethodRegistry.tracer = self
+        self.customed_leaf_module = customed_leaf_module
+        self.customed_skipped_module = customed_skipped_module
+        if self.customed_skipped_module:
+            self.register_skipped_module()
+
+    def register_skipped_module(self):
+        if self.customed_skipped_module is not list:
+            self.customed_skipped_module = [self.customed_skipped_module]
+        method_list = []
+        for method_str in self.customed_skipped_module:
+            method = self._parse_method_string(method_str)
+            method_list.append(method)
+        register_skipped_method(method_list)
+
+    def _parse_method_string(self, method_string):
+        import importlib
+        pkg, string = method_string.split('.', 1)
+        mod = importlib.import_module(pkg)
+        while '.' in string:
+            module_name = string.split('.', 1)
+            mod = getattr(mod, module_name)
+
+        return mod
 
     def call_method(self, m: torch.nn.Module, name, method, args, kwargs):
         """Method that specifies the behavior of this ``Tracer`` when it
@@ -175,7 +212,7 @@ class CustomTracer(Tracer):
                 module_call_wrapper,
                 deduplicate=False)
 
-            for name, value in TracedMethodRegistry.method_dict.items():
+            for name, value in UntracedMethodRegistry.method_dict.items():
                 wrapped = value['wrapped']
                 patcher.patch_method(
                     value['mod'], name, wrapped, deduplicate=False)
@@ -196,15 +233,43 @@ class CustomTracer(Tracer):
 
     def is_skipped_method(self, m):
         mods = tuple(value['mod']
-                     for value in TracedMethodRegistry.method_dict.values())
+                     for value in UntracedMethodRegistry.method_dict.values())
         custom = isinstance(m, mods)
         return custom
 
+    # def is_leaf_module(self, m: torch.nn.Module,
+    #                    module_qualified_name: str) -> bool:
+    #     # return super().is_leaf_module(m, module_qualified_name)
+    #     leaf = super().is_leaf_module(m, module_qualified_name)
+    #     return leaf
     def is_leaf_module(self, m: torch.nn.Module,
                        module_qualified_name: str) -> bool:
-        # return super().is_leaf_module(m, module_qualified_name)
-        leaf = super().is_leaf_module(m, module_qualified_name)
-        return leaf
+        """A method to specify whether a given ``nn.Module`` is a "leaf"
+        module.
+
+        Leaf modules are the atomic units that appear in
+        the IR, referenced by ``call_module`` calls. By default,
+        Modules in the PyTorch standard library namespace (torch.nn)
+        are leaf modules. All other modules are traced through and
+        their constituent ops are recorded, unless specified otherwise
+        via this parameter.
+        Args:
+            m (Module): The module being queried about
+            module_qualified_name (str): The path to root of this module.
+                For example, if you have a module hierarchy where submodule
+                ``foo`` contains submodule ``bar``, which contains submodule
+                ``baz``, that module will appear with the qualified name
+                ``foo.bar.baz`` here.
+        """
+        if self.customed_leaf_module and isinstance(m,
+                                                    self.customed_leaf_module):
+            return True
+
+        if hasattr(m, '_is_leaf_module') and m._is_leaf_module:
+            return True
+
+        return m.__module__.startswith('torch.nn') and not isinstance(
+            m, torch.nn.Sequential)
 
     def iter(self, obj: 'Proxy') -> Iterator:
         """Called when a proxy object is being iterated over, such as when used
---
+++ mmrazor/models/utils/fx_model_wrapper.py
@@ -0,0 +1,46 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch.nn as nn
+from mmcls.models import BaseClassifier
+from mmdet.models.detectors.base import BaseDetector
+from mmengine.model import BaseModule
+
+from mmrazor.registry import MODELS
+from .custom_tracer import CustomTracer
+
+
+@MODELS.register_module()
+class FXModelWrapper(BaseModule):
+
+    def __init__(self,
+                 model,
+                 customed_skipped_method=None,
+                 mode='loss') -> None:
+        super().__init__()
+        if isinstance(model, dict):
+            self.model = MODELS.build(model)
+        elif isinstance(model, nn.Module):
+            self.model = model
+
+        assert isinstance(self.model, BaseModule)
+        assert mode in ['tensor', 'loss', 'predict']
+        self.mode = mode
+        self.tracer = CustomTracer(
+            customed_skipped_module=customed_skipped_method)
+        self.model = self.tracer.trace(self.model)
+
+    def forward(self, inputs, data_samples):
+        if self.mode == 'loss':
+            return self.model.loss(inputs, data_samples)
+        elif self.mode == 'predict':
+            return self.model.predict(inputs, data_samples)
+        elif self.mode == 'tensor':
+            return self.tensor(inputs, data_samples)
+        else:
+            raise RuntimeError(f'Invalid mode "{self.mode}". '
+                               'Only supports loss, predict and tensor mode')
+
+    def tensor(self, inputs, data_samples):
+        if isinstance(self.model, BaseDetector):
+            return self.model._forward(inputs, data_samples)
+        elif isinstance(self.model, BaseClassifier):
+            raise NotImplementedError('TODO')
