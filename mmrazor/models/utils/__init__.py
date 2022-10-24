10fe8eb8905336d482ffe4e7de6e3b618d6c5956
huangpengsheng <huangpengsheng@sensetime.com>
2022-10-24 15:32:26 +0800

    updated

--- mmrazor/models/utils/__init__.py
+++ mmrazor/models/utils/__init__.py
@@ -1,5 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .custom_tracer import CustomTracer, TracedMethodRegistry
+from .custom_tracer import (CustomTracer, UntracedMethodRegistry,
+                            register_skipped_method)
+from .fx_model_wrapper import FXModelWrapper
 from .make_divisible import make_divisible
 from .misc import add_prefix
 from .optim_wrapper import reinitialize_optim_wrapper_count_status
@@ -8,5 +10,5 @@ from .utils import get_module_device, set_requires_grad
 __all__ = [
     'add_prefix', 'reinitialize_optim_wrapper_count_status', 'make_divisible',
     'get_module_device', 'set_requires_grad', 'CustomTracer',
-    'TracedMethodRegistry'
+    'UntracedMethodRegistry', 'FXModelWrapper', 'register_skipped_method'
 ]
