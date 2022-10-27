cc6719ffb27a7cb55ee4419ba30f7e14aa48d97e
FreakieHuang <frank0huang@foxmail.com>
2022-10-27 23:40:42 +0800

    for RFC

---
+++ mmrazor/models/fx_models/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .fx_model_wrapper import FXModelWrapper
+
+__all__ = ['FXModelWrapper']
--- mmrazor/models/utils/__init__.py
+++ mmrazor/models/utils/__init__.py
@@ -1,7 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .custom_tracer import (CustomTracer, UntracedMethodRegistry,
-                            register_skipped_method)
-from .fx_model_wrapper import FXModelWrapper
 from .make_divisible import make_divisible
 from .misc import add_prefix
 from .optim_wrapper import reinitialize_optim_wrapper_count_status
@@ -9,6 +6,5 @@ from .utils import get_module_device, set_requires_grad
 
 __all__ = [
     'add_prefix', 'reinitialize_optim_wrapper_count_status', 'make_divisible',
-    'get_module_device', 'set_requires_grad', 'CustomTracer',
-    'UntracedMethodRegistry', 'FXModelWrapper', 'register_skipped_method'
+    'get_module_device', 'set_requires_grad'
 ]
