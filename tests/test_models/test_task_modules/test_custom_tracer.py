426a0004d0af0c7ab88c1dffec1a81ab7f12b85b
FreakieHuang <frank0huang@foxmail.com>
2022-10-31 20:10:22 +0800

    add UT init

--- tests/test_models/test_task_modules/test_custom_tracer.py
+++ tests/test_models/test_task_modules/test_custom_tracer.py
@@ -1,35 +1,17 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from unittest import TestCase
+class testCustomTracer:
 
-from mmrazor.models.task_modules import CustomTracer, UntracedMethodRegistry
-from mmrazor.testing import ConvBNReLU
-
-
-class testCustomTracer(TestCase):
-
-    def test_init(self):
-        tracer = CustomTracer()
-        assert tracer.skipped_methods.__len__() == 0
-
-    def test_trace(self):
-        tracer = CustomTracer()
-        model = ConvBNReLU(3, 3, norm_cfg=dict(type='BN'))
-        graph = tracer.trace(model)  # noqa: F841
-
-    def test_auto_skip_call_module(self):
+    def test_init():
         pass
 
-    def test_auto_skip_call_method(self):
+    def test_trace():
         pass
 
-    def test_configurable_skipped_methods(self):
+    def test_auto_skip_call_module():
         pass
 
+    def test_auto_skip_call_method():
+        pass
 
-class testUntracedMethodRgistry(TestCase):
-
-    def test_init(self):
-        self.assertEqual(len(UntracedMethodRegistry.method_dict), 0)
-
-    def test_add_method(self):
+    def test_configurable_skipped_methods():
         pass
