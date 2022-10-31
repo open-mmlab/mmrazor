1180cdaf835b947dc26ab276b48e3d28c274265b
FreakieHuang <frank0huang@foxmail.com>
2022-10-31 20:10:22 +0800

    add UT init

---
+++ tests/test_models/test_task_modules/test_custom_tracer.py
@@ -0,0 +1,17 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+class testCustomTracer:
+
+    def test_init():
+        pass
+
+    def test_trace():
+        pass
+
+    def test_auto_skip_call_module():
+        pass
+
+    def test_auto_skip_call_method():
+        pass
+
+    def test_configurable_skipped_methods():
+        pass
