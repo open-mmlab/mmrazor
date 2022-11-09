c73e4775ec92f93ce4f97bd5b91fc30592c42da1
FreakieHuang <frank0huang@foxmail.com>
2022-11-09 22:57:43 +0800

    update UTs init

---
+++ tests/test_models/test_observers/test_observer.py
@@ -0,0 +1,38 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from unittest import TestCase
+
+import torch.nn as nn
+
+
+class ToyModel(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        # TODO
+
+
+class TestMinMaxObserver(TestCase):
+    """TODO.
+
+    Args:
+        TestCase (_type_): _description_
+    """
+
+    def test_init(self):
+        pass
+
+    def test_prepare(self):
+        pass
+
+    def test_convert(self):
+        pass
+
+    def test_states(self):
+        pass
+
+    def test_forward(self):
+        pass
+
+
+class TestLSQObserver(TestMinMaxObserver):
+    pass
