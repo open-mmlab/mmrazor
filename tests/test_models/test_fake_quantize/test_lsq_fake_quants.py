af12bac29987076ac9f62f3867bf968b31c7ddf8
FreakieHuang <frank0huang@foxmail.com>
2022-11-10 20:10:19 +0800

    fix review comments

---
+++ tests/test_models/test_fake_quantize/test_lsq_fake_quants.py
@@ -0,0 +1,23 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from unittest import TestCase
+
+
+class TestLearnableFakeQuantize(TestCase):
+
+    def test_init():
+        pass
+
+    def test_repr():
+        pass
+
+    def test_calculate_qparams():
+        pass
+
+    def test_forward():
+        pass
+
+    def test_load_state_dict():
+        pass
+
+    def test_save_state_dict():
+        pass
