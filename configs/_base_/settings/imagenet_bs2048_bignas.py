fb3caf6b1378ad4759e41911a34ad1ac2257c207
gaoyang07 <1546308416@qq.com>
2022-11-17 20:43:34 +0800

    arrange configs of bignas

--- configs/_base_/settings/imagenet_bs2048_bignas.py
+++ configs/_base_/settings/imagenet_bs2048_bignas.py
@@ -1,67 +1,346 @@
-default_scope = 'mmcls'
+# dataset settings
+dataset_type = 'mmcls.ImageNet'
 
-# !dataset config
-# ==========================================================================
-use_mc = True
-if use_mc:
-    file_client_args = dict(
-        backend='memcached',
-        server_list_cfg='/mnt/lustre/share/memcached_client/server_list.conf',
-        client_cfg='/mnt/lustre/share/memcached_client/client.conf')
-else:
-    file_client_args = dict(backend='disk')
-
-dataset_type = 'ImageNet'
-preprocess_cfg = dict(
+# data preprocessor
+data_preprocessor = dict(
+    type='mmcls.ClsDataPreprocessor',
     # RGB format normalization parameters
     mean=[123.675, 116.28, 103.53],
     std=[58.395, 57.12, 57.375],
     # convert image from BGR to RGB
     to_rgb=True,
-)
-data_preprocessor = dict(
-    type='ImgDataPreprocessor',
-    # RGB format normalization parameters
-    mean=preprocess_cfg['mean'],
-    std=preprocess_cfg['std'],
-    # convert image from BGR to RGB
-    bgr_to_rgb=preprocess_cfg['to_rgb'],
-)
+    num_classes=1000,
+    batch_augments=dict(
+        augments=[
+            dict(type='mmcls.Mixup', alpha=0.2),
+            dict(type='mmcls.CutMix', alpha=1.0)
+        ],
+        probs=[0.5, 0.5]))
 
-bgr_mean = preprocess_cfg['mean'][::-1]
-bgr_std = preprocess_cfg['std'][::-1]
+bgr_mean = data_preprocessor['mean'][::-1]
+bgr_std = data_preprocessor['std'][::-1]
+
+extra_params = dict(
+    translate_const=int(224 * 0.45),
+    img_mean=tuple(round(x) for x in data_preprocessor['mean']),
+)
+policies = [
+    [
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.8,
+            magnitude=1,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.ShearY',
+            prob=0.8,
+            magnitude=4,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.Color',
+            prob=0.4,
+            magnitude=9,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.6,
+            magnitude=3,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.Color',
+            prob=0.4,
+            magnitude=1,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.RotateV2',
+            prob=0.6,
+            magnitude=8,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.SolarizeV2',
+            prob=0.8,
+            magnitude=3,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.4,
+            magnitude=7,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.SolarizeV2',
+            prob=0.4,
+            magnitude=2,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.SolarizeV2',
+            prob=0.6,
+            magnitude=2,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.Color',
+            prob=0.2,
+            magnitude=0,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.8,
+            magnitude=8,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.4,
+            magnitude=8,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.SolarizeAddV2',
+            prob=0.8,
+            magnitude=3,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.ShearX',
+            prob=0.2,
+            magnitude=9,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.RotateV2',
+            prob=0.6,
+            magnitude=8,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.Color',
+            prob=0.6,
+            magnitude=1,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=1.0,
+            magnitude=2,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.InvertV2',
+            prob=0.4,
+            magnitude=9,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.RotateV2',
+            prob=0.6,
+            magnitude=0,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=1.0,
+            magnitude=9,
+            extra_params=extra_params),
+        dict(type='ShearY', prob=0.6, magnitude=3, extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.Color',
+            prob=0.4,
+            magnitude=7,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.6,
+            magnitude=0,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.PosterizeV2',
+            prob=0.4,
+            magnitude=6,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.AutoContrastV2',
+            prob=0.4,
+            magnitude=7,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.SolarizeV2',
+            prob=0.6,
+            magnitude=8,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.Color',
+            prob=0.6,
+            magnitude=9,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.SolarizeV2',
+            prob=0.2,
+            magnitude=4,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.RotateV2',
+            prob=0.8,
+            magnitude=9,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.RotateV2',
+            prob=1.0,
+            magnitude=7,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.TranslateYRel',
+            prob=0.8,
+            magnitude=9,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.ShearX',
+            prob=0.0,
+            magnitude=0,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.SolarizeV2',
+            prob=0.8,
+            magnitude=4,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.ShearY',
+            prob=0.8,
+            magnitude=0,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.Color',
+            prob=0.6,
+            magnitude=4,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.Color',
+            prob=1.0,
+            magnitude=0,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.RotateV2',
+            prob=0.6,
+            magnitude=2,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.8,
+            magnitude=4,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.0,
+            magnitude=8,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=1.0,
+            magnitude=4,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.AutoContrastV2',
+            prob=0.6,
+            magnitude=2,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.ShearY',
+            prob=0.4,
+            magnitude=7,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.SolarizeAddV2',
+            prob=0.6,
+            magnitude=7,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.PosterizeV2',
+            prob=0.8,
+            magnitude=2,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.SolarizeV2',
+            prob=0.6,
+            magnitude=10,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.SolarizeV2',
+            prob=0.6,
+            magnitude=8,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.EqualizeV2',
+            prob=0.6,
+            magnitude=1,
+            extra_params=extra_params),
+    ],
+    [
+        dict(
+            type='mmcls.Color',
+            prob=0.8,
+            magnitude=6,
+            extra_params=extra_params),
+        dict(
+            type='mmcls.RotateV2',
+            prob=0.4,
+            magnitude=5,
+            extra_params=extra_params),
+    ],
+]
 
 train_pipeline = [
-    dict(type='LoadImageFromFile', file_client_args=file_client_args),
-    dict(
-        type='RandomResizedCrop',
-        scale=224,
-        backend='pillow',
-        interpolation='bicubic'),
-    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
-    # TODO(shiguang): align with gml.AutoAugmentV2
-    dict(
-        type='RandAugment',
-        policies='timm_increasing',
-        num_policies=2,
-        total_level=10,
-        magnitude_level=7,
-        magnitude_std=0.5,
-        hparams=dict(
-            pad_val=[round(x) for x in bgr_mean], interpolation='bicubic')),
-    dict(type='PackClsInputs'),
+    dict(type='mmcls.LoadImageFromFile'),
+    dict(type='mmcls.RandomResizedCrop', scale=224, backend='pillow'),
+    dict(type='mmcls.RandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='mmcls.AutoAugmentV2', policies=policies),
+    dict(type='mmcls.PackClsInputs'),
 ]
 
 test_pipeline = [
-    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='mmcls.LoadImageFromFile'),
     dict(
-        type='ResizeEdge',
+        type='mmcls.ResizeEdge',
         scale=256,
         edge='short',
         backend='pillow',
-        interpolation='bicubic'),
-    dict(type='CenterCrop', crop_size=224),
-    dict(type='PackClsInputs')
+        interpolation='bilinear'),
+    dict(type='mmcls.CenterCrop', crop_size=224),
+    dict(type='mmcls.PackClsInputs')
 ]
 
 train_dataloader = dict(
@@ -73,7 +352,7 @@ train_dataloader = dict(
         ann_file='meta/train.txt',
         data_prefix='train',
         pipeline=train_pipeline),
-    sampler=dict(type='RepeatAugSampler', shuffle=True),
+    sampler=dict(type='mmcls.RepeatAugSampler', shuffle=True),
     persistent_workers=True,
 )
 
@@ -86,23 +365,23 @@ val_dataloader = dict(
         ann_file='meta/val.txt',
         data_prefix='val',
         pipeline=test_pipeline),
-    sampler=dict(type='DefaultSampler', shuffle=False),
+    sampler=dict(type='mmcls.DefaultSampler', shuffle=False),
     persistent_workers=True,
 )
-val_evaluator = dict(type='Accuracy', topk=(1, 5))
+val_evaluator = dict(type='mmcls.Accuracy', topk=(1, 5))
 
 # If you want standard test, please manually configure the test dataset
 test_dataloader = val_dataloader
 test_evaluator = val_evaluator
-# ==========================================================================
-# !schedule config
-# ==========================================================================
-# ! grad clip by value is not supported!
+
+# optimizer
 num_samples = 2
 optim_wrapper = dict(
-    optimizer=dict(type='SGD', lr=0.8, momentum=0.9, weight_decay=0.00001),
-    paramwise_cfg=dict(bias_decay_mult=0., norm_decay_mult=0.),  #
-    accumulative_counts=num_samples + 2)  # num_samples + 2
+    optimizer=dict(
+        type='SGD', lr=0.8, momentum=0.9, weight_decay=0.00001, nesterov=True),
+    clip_grad=dict(clip_value=1.0),
+    paramwise_cfg=dict(bias_decay_mult=0., norm_decay_mult=0.),
+    accumulative_counts=num_samples + 2)
 
 # learning policy
 max_epochs = 360
@@ -111,8 +390,8 @@ param_scheduler = [
     # warm up learning rate scheduler
     dict(
         type='LinearLR',
-        start_factor=0.0001,
-        by_epoch=True,
+        start_factor=0.001,
+        by_epoch=False,
         begin=0,
         end=warmup_epochs,
         # update by iter
--- configs/nas/mmcls/bignas/attentive_mobilenet_supernet_bs512_rsb_a12.py
+++ configs/nas/mmcls/bignas/attentive_mobilenet_supernet_bs512_rsb_a12.py
@@ -1,112 +1,15 @@
-default_scope = 'mmcls'
-
-# !dataset config
-# ==========================================================================
-use_ceph = True
-if use_ceph:
-    file_client_args = dict(
-        backend='petrel',
-        path_mapping=dict({
-            './data/imagenet':
-            's3://openmmlab/datasets/classification/imagenet',
-            'data/imagenet':
-            's3://openmmlab/datasets/classification/imagenet'
-        }))
-else:
-    file_client_args = dict(backend='disk')
-
-dataset_type = 'ImageNet'
-preprocess_cfg = dict(
-    # RGB format normalization parameters
-    mean=[123.675, 116.28, 103.53],
-    std=[58.395, 57.12, 57.375],
-    # convert image from BGR to RGB
-    to_rgb=True,
-)
-data_preprocessor = dict(
-    type='ImgDataPreprocessor',
-    # RGB format normalization parameters
-    mean=preprocess_cfg['mean'],
-    std=preprocess_cfg['std'],
-    # convert image from BGR to RGB
-    bgr_to_rgb=preprocess_cfg['to_rgb'],
-)
-
-bgr_mean = preprocess_cfg['mean'][::-1]
-bgr_std = preprocess_cfg['std'][::-1]
-
-train_pipeline = [
-    dict(type='LoadImageFromFile', file_client_args=file_client_args),
-    dict(
-        type='RandomResizedCrop',
-        scale=224,
-        backend='pillow',
-        interpolation='bicubic'),
-    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
-    dict(
-        type='RandAugment',
-        policies='timm_increasing',
-        num_policies=2,
-        total_level=10,
-        magnitude_level=7,
-        magnitude_std=0.5,
-        hparams=dict(
-            pad_val=[round(x) for x in bgr_mean], interpolation='bicubic')),
-    dict(type='PackClsInputs'),
+_base_ = [
+    'mmcls::_base_/default_runtime.py',
+    'mmrazor::_base_/settings/imagenet_bs2048_bignas.py',
+    'mmrazor::_base_/nas_backbones/bignas_mobilenetv3_supernet.py',
 ]
 
-test_pipeline = [
-    dict(type='LoadImageFromFile', file_client_args=file_client_args),
-    dict(
-        type='ResizeEdge',
-        scale=236,
-        edge='short',
-        backend='pillow',
-        interpolation='bicubic'),
-    dict(type='CenterCrop', crop_size=224),
-    dict(type='PackClsInputs')
-]
-
-train_dataloader = dict(
-    batch_size=64,
-    num_workers=16,
-    dataset=dict(
-        type=dataset_type,
-        data_root='data/imagenet',
-        ann_file='meta/train.txt',
-        data_prefix='train',
-        pipeline=train_pipeline),
-    sampler=dict(type='RepeatAugSampler', shuffle=True),
-    persistent_workers=True,
-)
-
-val_dataloader = dict(
-    batch_size=64,
-    num_workers=16,
-    dataset=dict(
-        type=dataset_type,
-        data_root='data/imagenet',
-        ann_file='meta/val.txt',
-        data_prefix='val',
-        pipeline=test_pipeline),
-    sampler=dict(type='DefaultSampler', shuffle=False),
-    persistent_workers=True,
-)
-val_evaluator = dict(type='Accuracy', topk=(1, 5))
-
-# If you want standard test, please manually configure the test dataset
-test_dataloader = val_dataloader
-test_evaluator = val_evaluator
-
-# !supernet config
-# ==========================================================================
 supernet = dict(
     _scope_='mmrazor',
     type='SearchableImageClassifier',
     backbone=dict(
         type='AttentiveMobileNet',
-        first_out_channels_range=[16, 24, 8],
-        last_out_channels_range=[1792, 1984, 1984 - 1792],
+        arch_setting=_base_.arch_setting,
         dropout_stages=6,
         act_cfg=dict(type='Swish')),
     neck=dict(type='mmcls.GlobalAveragePooling'),
@@ -121,10 +24,6 @@ supernet = dict(
             mode='original',
             loss_weight=1.0),
         topk=(1, 5)),
-    train_cfg=dict(augments=[
-        dict(type='mmcls.Mixup', alpha=0.2, num_classes=1000),
-        dict(type='mmcls.CutMix', alpha=1.0, num_classes=1000)
-    ]),
     input_resizer_cfg=dict(
         input_resizer=dict(type='DynamicInputResizer'),
         mutable_shape=dict(
@@ -132,16 +31,13 @@ supernet = dict(
             value_list=[[192, 192], [224, 224], [256, 256], [288, 288]],
             default_value=[224, 224])))
 
-# !algorithm config
-# ==========================================================================
-num_samples = 2
 model = dict(
     _scope_='mmrazor',
     type='BigNAS',
-    num_samples=num_samples,
+    num_samples=_base_.num_samples,
     drop_prob=0.2,
     architecture=supernet,
-    data_preprocessor=data_preprocessor,
+    data_preprocessor=_base_.data_preprocessor,
     distiller=dict(
         type='ConfigurableDistiller',
         teacher_recorders=dict(
@@ -155,95 +51,18 @@ model = dict(
                 preds_S=dict(recorder='fc', from_student=True),
                 preds_T=dict(recorder='fc', from_student=False)))),
     mutators=dict(
-        channel_mutator=dict(type='BigNASChannelMutator'),
+        channel_mutator=dict(
+            type='mmrazor.OneShotChannelMutator',
+            channel_unit_cfg={
+                'type': 'OneShotMutableChannelUnit',
+                'default_args': {
+                    'unit_predefined': True
+                }
+            },
+            parse_cfg={'type': 'Predefined'}),
         value_mutator=dict(type='DynamicValueMutator')))
 
-# bug, must have `mmrazor` prefix
 model_wrapper_cfg = dict(
     type='mmrazor.BigNASDDP',
     broadcast_buffers=False,
     find_unused_parameters=True)
-
-# !schedule config
-# ==========================================================================
-optim_wrapper = dict(
-    optimizer=dict(type='Lamb', lr=0.005, weight_decay=0.01),
-    paramwise_cfg=dict(bias_decay_mult=0., norm_decay_mult=0.),
-    accumulative_counts=num_samples + 2)
-# NOTE: `auto_scale_lr` is for automatically scaling LR,
-# based on the actual training batch size.
-auto_scale_lr = dict(base_batch_size=64 * 8)
-
-# learning policy
-param_scheduler = [
-    # warm up learning rate scheduler
-    dict(
-        type='LinearLR',
-        start_factor=0.0001,
-        by_epoch=True,
-        begin=0,
-        end=5,
-        # update by iter
-        convert_to_iter_based=True),
-    # main learning rate scheduler
-    dict(
-        type='CosineAnnealingLR',
-        T_max=595,
-        eta_min=1.0e-6,
-        by_epoch=True,
-        begin=5,
-        end=600)
-]
-
-# train, val, test setting
-train_cfg = dict(by_epoch=True, max_epochs=600, val_interval=1)
-val_cfg = dict(type='mmrazor.AutoSlimValLoop', calibrated_sample_nums=4096)
-test_cfg = dict(type='mmrazor.AutoSlimTestLoop', calibrated_sample_nums=4096)
-
-# !runtime config
-# ==========================================================================
-# configure default hooks
-default_hooks = dict(
-    # record the time of every iteration.
-    timer=dict(type='IterTimerHook'),
-
-    # print log every 100 iterations.
-    logger=dict(type='LoggerHook', interval=100),
-
-    # enable the parameter scheduler.
-    param_scheduler=dict(type='ParamSchedulerHook'),
-
-    # save checkpoint per epoch.
-    checkpoint=dict(
-        type='CheckpointHook', interval=1, max_keep_ckpts=10,
-        save_best='auto'),
-
-    # set sampler seed in distributed evrionment.
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-
-    # validation results visualization, set True to enable it.
-    visualization=dict(type='VisualizationHook', enable=False),
-)
-
-# configure environment
-env_cfg = dict(
-    # whether to enable cudnn benchmark
-    cudnn_benchmark=False,
-    # set multi process parameters
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
-    # set distributed parameters
-    dist_cfg=dict(backend='nccl'),
-)
-
-# set visualizer
-vis_backends = [dict(type='LocalVisBackend')]
-visualizer = dict(type='ClsVisualizer', vis_backends=vis_backends)
-
-# load from which checkpoint
-load_from = None
-
-# whether to resume training from the loaded checkpoint
-resume = False
-
-# set log level
-log_level = 'DEBUG'
