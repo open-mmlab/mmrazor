8e8db24c5f8fd0e8ff9b4de2a48ef77b0971f31a
Xianpan Zhou <1239068645@qq.com>
2022-12-06 12:37:37 +0800

    Make changes accroding to PR review.

--- tools/model_converters/convert_kd_ckpt_to_student.py
+++ tools/model_converters/convert_kd_ckpt_to_student.py
@@ -7,10 +7,9 @@ import torch
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Process a checkpoint to be published')
+        description='Convert KD checkpoint to student-only checkpoint')
     parser.add_argument('checkpoint', help='input checkpoint filename')
-    parser.add_argument(
-        '--model-only', action='store_true', help='only save model')
+    parser.add_argument('--out-path', help='save checkpoint path')
     parser.add_argument(
         '--inplace', action='store_true', help='replace origin ckpt')
     args = parser.parse_args()
@@ -21,15 +20,15 @@ def main():
     args = parse_args()
     checkpoint = torch.load(args.checkpoint, map_location='cpu')
     new_state_dict = dict()
+    new_meta = checkpoint['meta']
 
     for key, value in checkpoint['state_dict'].items():
         if key.startswith('architecture.'):
             new_key = key.replace('architecture.', '')
             new_state_dict[new_key] = value
 
-    if args.model_only:
-        checkpoint = dict()
-
+    checkpoint = dict()
+    checkpoint['meta'] = new_meta
     checkpoint['state_dict'] = new_state_dict
 
     if args.inplace:
@@ -37,7 +36,10 @@ def main():
     else:
         ckpt_path = Path(args.checkpoint)
         ckpt_name = ckpt_path.stem
-        ckpt_dir = ckpt_path.parent
+        if args.out_path:
+            ckpt_dir = Path(args.out_path)
+        else:
+            ckpt_dir = ckpt_path.parent
         new_ckpt_path = ckpt_dir / f'{ckpt_name}_student.pth'
         torch.save(checkpoint, new_ckpt_path)
 
