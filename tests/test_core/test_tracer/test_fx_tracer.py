3203450eecde827f22456ebc239dfd34b025194e
liukai <liukai@pjlab.org.cn>
2022-12-07 17:09:24 +0800

    fix bug when test with torch<1.12

--- tests/test_core/test_tracer/test_fx_tracer.py
+++ tests/test_core/test_tracer/test_fx_tracer.py
@@ -3,16 +3,19 @@ import unittest
 from functools import partial
 
 from mmcls.models.classifiers.image import ImageClassifier
-from torch.fx.graph_module import GraphModule
 
+from mmrazor.utils import get_placeholder
+
+try:
+    from torch.fx.graph_module import GraphModule
+except ImportError:
+    GraphModule = get_placeholder('torch>=1.12')
+import torch
+
+from mmrazor import digit_version
 from mmrazor.models.task_modules.demo_inputs import DefaultDemoInput
 from mmrazor.models.task_modules.tracer.fx_tracer import FxTracer
 from ...data.models import UntracableModel
-from ...utils.set_log_level import set_log_level
-
-# import torch
-
-set_log_level('debug')
 
 MODELS = [
     UntracableModel,
@@ -29,33 +32,37 @@ MODELS = [
 class TestFxTracer(unittest.TestCase):
 
     def test_model(self):
+        if digit_version(torch.__version__) < digit_version('1.12.0'):
+            self.skipTest('version of torch < 1.12.0')
+
         for Model in MODELS:
-            model = Model()
+            with self.subTest(model=Model):
+                model = Model()
 
-            tracer = FxTracer()
-            demo_input = DefaultDemoInput()
-            inputs = demo_input.get_data(model)
+                tracer = FxTracer()
+                demo_input = DefaultDemoInput()
+                inputs = demo_input.get_data(model)
 
-            if isinstance(inputs, dict):
-                # args = copy.copy(inputs)
-                # args.pop('inputs')
-                # args['mode'] = 'tensor'
-                args = {'mode': 'tensor'}
-                torch_graph = tracer.trace(model, concrete_args=args)
-            else:
-                torch_graph = tracer.trace(model)
-            print(model)
+                if isinstance(inputs, dict):
+                    # args = copy.copy(inputs)
+                    # args.pop('inputs')
+                    # args['mode'] = 'tensor'
+                    args = {'mode': 'tensor'}
+                    torch_graph = tracer.trace(model, concrete_args=args)
+                else:
+                    torch_graph = tracer.trace(model)
+                print(model)
 
-            print(torch_graph)
+                print(torch_graph)
 
-            graph_module = GraphModule(model, torch_graph)
-            print(graph_module)
-            print(graph_module.code)
+                graph_module = GraphModule(model, torch_graph)
+                print(graph_module)
+                print(graph_module.code)
 
-            inputs = demo_input.get_data(model)
-            if isinstance(inputs, dict):
-                inputs['mode_1'] = inputs['mode']
-                inputs.pop('mode')
-                graph_module(**inputs)
-            else:
-                graph_module(inputs)
+                inputs = demo_input.get_data(model)
+                if isinstance(inputs, dict):
+                    inputs['mode_1'] = inputs['mode']
+                    inputs.pop('mode')
+                    graph_module(**inputs)
+                else:
+                    graph_module(inputs)
