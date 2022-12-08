653d398570e448f1f3ce99542b3679e353efb860
LSH <599194771@qq.com>
2022-12-08 20:55:45 +0800

    add test for MinMaxFloorObserver

--- mmrazor/models/observers/__init__.py
+++ mmrazor/models/observers/__init__.py
@@ -1,7 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .lsq_observer import LSQObserver
 from .minmax import EMAMinMaxObserver, MinMaxObserver
-from .mse import MSEObserver
 from .minmaxfloor_observer import MinMaxFloorObserver
+from .mse import MSEObserver
 
-__all__ = ['MinMaxObserver', 'MSEObserver', 'EMAMinMaxObserver', 'LSQObserver', 'MinMaxFloorObserver']
+__all__ = [
+    'MinMaxObserver', 'MSEObserver', 'EMAMinMaxObserver', 'LSQObserver',
+    'MinMaxFloorObserver'
+]
