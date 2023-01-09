53fbe1a9d1389a0d07b66b69353c2d1d9761a049
niko <415905716@qq.com>
2023-01-09 20:23:13 +0800

    unitest

--- tests/test_models/test_algorithms/test_mm_architecture.py
+++ tests/test_models/test_algorithms/test_mm_architecture.py
@@ -1,13 +1,18 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from unittest import TestCase
 
+import torch
 import torch.nn as nn
+from torch.fx import GraphModule
+import os
 
-from mmengine import ConfigDict
 from mmengine.model import BaseModel
 
 from mmrazor.models.algorithms import MMArchitectureQuant
 from mmrazor.registry import MODELS
+
+import shutil
+import tempfile
 
 @MODELS.register_module()
 class ToyQuantModel(BaseModel):
@@ -32,22 +37,29 @@ class ToyQuantModel(BaseModel):
 
 
 class TestMMArchitectureQuant(TestCase):
-    def test_init(self):
-        # _base_ = ['mmcls::resnet/resnet18_8xb32_in1k.py']
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
+        filename = 'fp_model.pth'
+        filename = os.path.join(self.temp_dir, filename)
+        toymodel = ToyQuantModel()
+        torch.save(toymodel.state_dict(), filename)
+        
         global_qconfig = dict(
             w_observer=dict(type='mmrazor.PerChannelMinMaxObserver'),
             a_observer=dict(type='mmrazor.MovingAverageMinMaxObserver'),
             w_fake_quant=dict(type='mmrazor.FakeQuantize'),
             a_fake_quant=dict(type='mmrazor.FakeQuantize'),
             w_qscheme=dict(
-                qdtype='qint8', bit=8, is_symmetry=True, is_symmetric_range=True),
+                qdtype='qint8', bit=8, 
+                is_symmetry=True, is_symmetric_range=True),
             a_qscheme=dict(
-                qdtype='quint8', bit=8, is_symmetry=True, averaging_constant=0.1),
+                qdtype='quint8', bit=8, 
+                is_symmetry=True, averaging_constant=0.1),
             )
-        alg_kwargs = ConfigDict(
+        alg_kwargs = dict(
             type = 'mmrazor.MMArchitectureQuant',
             architecture=dict(type='ToyQuantModel'),
-            # architecture=dict(type='mmcls.ResNet', depth=18),
+            float_checkpoint = 'fp_model.pth',
             quantizer=dict(
                 type='mmrazor.OpenVINOQuantizer',
                 global_qconfig=global_qconfig,
@@ -57,18 +69,30 @@ class TestMMArchitectureQuant(TestCase):
                         'mmcls.models.heads.ClsHead._get_loss',
                         'mmcls.models.heads.ClsHead._get_predictions'
                     ])))
-        toy_model=MODELS.build(alg_kwargs)
-        assert isinstance(toy_model, MMArchitectureQuant)
-        assert hasattr(toy_model, 'quantizer')
-        # _ = MMArchitectureQuant()
+        self.alg_kwargs = alg_kwargs
+        self.toy_model=MODELS.build(self.alg_kwargs)
+        
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
+        
+    def test_init(self):
+        assert isinstance(self.toy_model, MMArchitectureQuant)
+        assert hasattr(self.toy_model, 'quantizer')
             
     def test_sync_qparams(self):
-        # self.test_sync_qparams()
-        pass
+        mode = self.toy_model.forward_modes[0]
+        self.toy_model.sync_qparams(mode)
+        w_loss = self.toy_model.qmodels['loss'].conv.state_dict()['weight']
+        w_tensor = self.toy_model.qmodels['tensor'].conv.state_dict()['weight']
+        w_pred = self.toy_model.qmodels['predict'].conv.state_dict()['weight']
+        assert w_loss.equal(w_pred)
+        assert w_loss.equal(w_tensor)
     
     def test_build_qmodels(self):
+        for forward_modes in self.toy_model.forward_modes:
+            qmodels = self.toy_model.qmodels[forward_modes]
+            assert isinstance(qmodels, GraphModule)
         
-        pass
-    
     def test_calibrate_step(self):
+        # TODO
         pass
