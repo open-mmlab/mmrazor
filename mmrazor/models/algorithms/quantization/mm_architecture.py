693a76f79dd583dc9a31d896526591165070b943
niko <415905716@qq.com>
2023-01-13 14:59:57 +0800

    update docstring

--- mmrazor/models/algorithms/quantization/mm_architecture.py
+++ mmrazor/models/algorithms/quantization/mm_architecture.py
@@ -79,9 +79,10 @@ class MMArchitectureQuant(BaseAlgorithm):
         self.sync_qparams('predict')
 
     def sync_qparams(self, src_mode: str):
-        """Sync all quantize parameters in different `forward_modes`. We have
-        three modes to generate three graphs, but in training, only one graph
-        will be update, so we need to sync qparams in the other two graphs.
+        """Sync all quantize parameters in different `forward_modes`. We could 
+        have more than one forward mode to generate graphs, each mode will 
+        generate one graph. But in training, only one graph will be update, so 
+        we need to sync qparams in the other graphs.
 
         Args:
             src_mode (str): The modes of forward method.
@@ -220,9 +221,9 @@ class MMArchitectureQuantDDP(MMDistributedDataParallel):
 
     def sync_qparams(self, src: str):
         """Same as in 'MMArchitectureQuant'. Sync all quantize parameters in
-        different `forward_modes`. We have three modes to generate three
+        different `forward_modes`. We could have several modes to generate 
         graphs, but in training, only one graph will be update, so we need to
-        sync qparams in the other two graphs.
+        sync qparams on the other graphs.
 
         Args:
             src (str): The src modes of forward method.
