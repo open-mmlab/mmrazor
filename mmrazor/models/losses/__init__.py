a1c9426e5f1d6858d87a503154c50bdca594eb03
HIT-cwh <2892770585@qq.com>
2023-02-14 14:41:53 +0800

    add fgd

--- mmrazor/models/losses/__init__.py
+++ mmrazor/models/losses/__init__.py
@@ -8,6 +8,7 @@ from .dafl_loss import ActivationLoss, InformationEntropyLoss, OnehotLikeLoss
 from .decoupled_kd import DKDLoss
 from .factor_transfer_loss import FTLoss
 from .fbkd_loss import FBKDLoss
+from .fgd_loss import FGDLoss
 from .kd_soft_ce_loss import KDSoftCELoss
 from .kl_divergence import KLDivergence
 from .l1_loss import L1Loss
@@ -22,5 +23,6 @@ __all__ = [
     'ChannelWiseDivergence', 'KLDivergence', 'AngleWiseRKD', 'DistanceWiseRKD',
     'WSLD', 'L2Loss', 'ABLoss', 'DKDLoss', 'KDSoftCELoss', 'ActivationLoss',
     'OnehotLikeLoss', 'InformationEntropyLoss', 'FTLoss', 'ATLoss', 'OFDLoss',
-    'L1Loss', 'FBKDLoss', 'CRDLoss', 'CrossEntropyLoss', 'PKDLoss', 'MGDLoss'
+    'L1Loss', 'FBKDLoss', 'CRDLoss', 'CrossEntropyLoss', 'PKDLoss', 'MGDLoss',
+    'FGDLoss'
 ]
