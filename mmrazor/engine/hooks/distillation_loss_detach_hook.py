2a6dd39a0bc75a828294e2cb29f2fdf723c1a5df
HIT-cwh <2892770585@qq.com>
2023-02-15 22:44:51 +0800

    fix bugs when resuming

--- mmrazor/engine/hooks/distillation_loss_detach_hook.py
+++ mmrazor/engine/hooks/distillation_loss_detach_hook.py
@@ -14,7 +14,7 @@ class DistillationLossDetachHook(Hook):
         self.detach_epoch = detach_epoch
 
     def before_train_epoch(self, runner) -> None:
-        if runner.epoch == self.detach_epoch:
+        if runner.epoch >= self.detach_epoch:
             model = runner.model
             # TODO: refactor after mmengine using model wrapper
             if is_model_wrapper(model):
