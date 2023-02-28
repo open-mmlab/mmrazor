e0fabc341dbc8b6b8df35ae1106c6d7dfd1dd601
sunyue1 <sunyue1@sensetime.com>
2023-02-28 14:58:34 +0800

    fix ofa yaml

--- configs/nas/mmcls/onceforall/ofa_mobilenet_subnet_8xb256_in1k.py
+++ configs/nas/mmcls/onceforall/ofa_mobilenet_subnet_8xb256_in1k.py
@@ -1,12 +1,15 @@
 _base_ = 'ofa_mobilenet_supernet_32xb64_in1k.py'
 
+_base_.supernet.data_preprocessor = _base_.data_preprocessor
+
 supernet = _base_.supernet
 
 model_cfg = dict(
     _scope_='mmrazor',
     type='sub_model',
     cfg=supernet,
-    fix_subnet='configs/nas/mmcls/onceforall/OFA_SUBNET_NOTE8_LAT22.yaml')
+    fix_subnet='configs/nas/mmcls/onceforall/OFA_SUBNET_NOTE8_LAT31.yaml')
+    # fix_subnet='configs/nas/mmcls/onceforall/OFA_SUBNET_NOTE8_LAT22.yaml')
 
 _base_.model = model_cfg
 _base_.model_wrapper_cfg = None
