24297cc80438108c27c662dd2a029e6fd93b1422
liukai <your_email@abc.example>
2023-03-09 10:03:28 +0800

    fix bug

--- mmrazor/models/task_modules/estimators/counters/op_counters/conv_layer_counter.py
+++ mmrazor/models/task_modules/estimators/counters/op_counters/conv_layer_counter.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import numpy as np
-import torch.nn as nn
 
+from mmrazor.models.architectures.dynamic_ops import DynamicConv2d
 from mmrazor.registry import TASK_UTILS
 from .base_counter import BaseCounter
 
@@ -66,7 +66,7 @@ class Conv3dCounter(ConvCounter):
 class DynamicConv2dCounter(ConvCounter):
 
     @staticmethod
-    def add_count_hook(module: nn.Conv2d, input, output):
+    def add_count_hook(module: DynamicConv2d, input, output):
         """Calculate FLOPs and params based on the dynamic channels of conv
         layers."""
         input = input[0]
@@ -76,12 +76,21 @@ class DynamicConv2dCounter(ConvCounter):
 
         kernel_dims = list(module.kernel_size)
 
-        out_channels = module.mutable_attrs['out_channels'].activated_channels
-        mutable_channel = list(
-            module.mutable_attrs['out_channels'].mutable_channels.values())
-        if hasattr(mutable_channel[0], 'activated_tensor_channels'):
-            out_channels = mutable_channel[0].activated_tensor_channels
-        in_channels = module.mutable_attrs['in_channels'].activated_channels
+        if 'out_channels' in module.mutable_attrs:
+            out_channels = module.mutable_attrs[
+                'out_channels'].activated_channels
+            mutable_channel = list(
+                module.mutable_attrs['out_channels'].mutable_channels.values())
+            if len(mutable_channel) > 0 and hasattr(
+                    mutable_channel[0], 'activated_tensor_channels'):
+                out_channels = mutable_channel[0].activated_tensor_channels
+        else:
+            out_channels = module.out_channels
+        if 'in_channels' in module.mutable_attrs:
+            in_channels = module.mutable_attrs[
+                'in_channels'].activated_channels
+        else:
+            in_channels = module.in_channels
 
         groups = module.groups
 
