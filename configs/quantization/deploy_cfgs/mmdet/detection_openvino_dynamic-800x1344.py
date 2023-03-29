019e43b674b65c2239775ba9061684d45ea22be2
whcao <41630003+HIT-cwh@users.noreply.github.com>
2023-03-29 20:17:14 +0800

    [Feature] Using rewriter in mmrazor when building qmodels. (#490)
    * add rewriter
    
    * add deploy_cfg arg
    
    * modify post_process_for_mmdeploy
    
    * fix bugs
    
    * add det config
    
    * replace deepcopy
    
    * pop detectors' forward

--- configs/quantization/deploy_cfgs/mmdet/detection_openvino_dynamic-800x1344.py
+++ configs/quantization/deploy_cfgs/mmdet/detection_openvino_dynamic-800x1344.py
@@ -44,7 +44,5 @@ deploy_cfg = dict(
         'mmdet.models.detectors.single_stage.SingleStageDetector.forward',
         'mmdet.models.detectors.two_stage.TwoStageDetector.forward',
         'mmdet.models.detectors.single_stage_instance_seg.'
-        'SingleStageInstanceSegmentor.forward',
-        'mmdet.models.dense_heads.base_dense_head.BaseDenseHead.'
-        'predict_by_feat'
+        'SingleStageInstanceSegmentor.forward'
     ])
