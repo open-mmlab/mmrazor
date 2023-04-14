c36dfb88c28c39d8707c88b0a80e446b4b2b4912
HIT-cwh <2892770585@qq.com>
2023-04-14 20:28:55 +0800

    fix qat configs

--- configs/quantization/qat/qat_openvino_resnet18_10e_8xb32_in1k.py
+++ configs/quantization/qat/qat_openvino_resnet18_10e_8xb32_in1k.py
@@ -4,10 +4,10 @@ resnet = _base_.model
 float_checkpoint = 'https://download.openmmlab.com/mmclassification/v0/resnet/resnet18_8xb32_in1k_20210831-fbbb1da6.pth'  # noqa: E501
 
 global_qconfig = dict(
-    w_observer=dict(type='mmrazor.LSQPerChannelObserver'),
-    a_observer=dict(type='mmrazor.LSQObserver'),
-    w_fake_quant=dict(type='mmrazor.LearnableFakeQuantize'),
-    a_fake_quant=dict(type='mmrazor.LearnableFakeQuantize'),
+    w_observer=dict(type='mmrazor.PerChannelMinMaxObserver'),
+    a_observer=dict(type='mmrazor.MovingAverageMinMaxObserver'),
+    w_fake_quant=dict(type='mmrazor.FakeQuantize'),
+    a_fake_quant=dict(type='mmrazor.FakeQuantize'),
     w_qscheme=dict(
         qdtype='qint8', bit=8, is_symmetry=True, is_symmetric_range=True),
     a_qscheme=dict(qdtype='quint8', bit=8, is_symmetry=True),
@@ -47,15 +47,16 @@ param_scheduler = dict(
 model_wrapper_cfg = dict(
     type='mmrazor.MMArchitectureQuantDDP',
     broadcast_buffers=False,
-    find_unused_parameters=True)
+    find_unused_parameters=False)
 
 # train, val, test setting
 train_cfg = dict(
     _delete_=True,
-    type='mmrazor.LSQEpochBasedLoop',
+    type='mmrazor.QATEpochBasedLoop',
     max_epochs=10,
     val_interval=1)
 val_cfg = dict(_delete_=True, type='mmrazor.QATValLoop')
-# test_cfg = val_cfg
 
+# Make sure the buffer such as min_val/max_val in saved checkpoint is the same
+# among different rank.
 default_hooks = dict(sync=dict(type='SyncBuffersHook'))
